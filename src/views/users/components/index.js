79b659b02891a8a4092ad1be632b676e2916ee73
OrangeM21 <orangemtony@gmail.com>
2020-03-22 23:41:40 +0800

    [update]准备组件分拆基础

---
+++ src/views/users/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar'
+export { default as Sidebar } from './Sidebar'
+export { default as AppMain } from './AppMain'
+export { default as BackPlayground } from './BackPlayground'
+export { default as ListTables } from './ListTables'
+export { default as Footer } from './Footer'
