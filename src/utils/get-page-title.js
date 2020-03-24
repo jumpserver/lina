e04c745ff648c04285600b079f1061c75f55c06c
OrangeM21 <orangemtony@gmail.com>
2020-03-24 11:27:27 +0800

    [fix]修改标题名

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -3,8 +3,8 @@ import defaultSettings from '@/settings'
 const title = defaultSettings.title || 'Vue Admin Template'
 
 export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
-  }
+  // if (pageTitle) {
+  //   return `${pageTitle} - ${title}`
+  // }
   return `${title}`
 }
