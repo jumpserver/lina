4140801eeece4652e8917642367eacca8926131c
jym503558564 <503558564@qq.com>
2020-04-02 15:52:39 +0800

    [Update] 修改资产列表组件

--- src/views/assets/AssetList.vue
+++ src/views/assets/AssetList.vue
@@ -1,13 +1,54 @@
 <template>
-  <h1>这是组件AssetList</h1>
+  <GenericListPage :table-config="tableConfig" :header-actions="headerActions" />
 </template>
 
 <script>
+import { GenericListPage } from '@/layout/components'
+import DetailFormatter from '@/components/DataTable/formatters/DetailFormatter'
+
 export default {
-  name: 'AssetList'
+  components: {
+    GenericListPage
+  },
+  data() {
+    return {
+      showTree: true,
+      tableConfig: {
+        url: '/api/v1/assets/assets/',
+        columns: [
+          {
+            prop: 'hostname',
+            label: this.$t('assets.hostname'),
+            formatter: DetailFormatter,
+            sortable: true,
+            route: 'AssetDetail'
+          },
+          {
+            prop: 'ip',
+            label: this.$t('assets.ip')
+          },
+          {
+            prop: 'hardware_info',
+            label: this.$t('assets.hardware')
+          },
+          {
+            prop: 'reachable',
+            label: this.$t('assets.reachable')
+          }
+        ],
+        tableActions: {
+          hasEdit: true,
+          editRoute: '404'
+        }
+      },
+      headerActions: {
+        createRoute: 'AssetCreate'
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
