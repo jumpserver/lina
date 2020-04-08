66970f64019a12402550f13c767aed2fbe57066a
OrangeM21 <orangemtony@gmail.com>
2020-04-08 14:28:33 +0800

    [update]更新时间设置

---
+++ src/i18n/date.js
@@ -0,0 +1,20 @@
+export default {
+  'en': {
+    short: {
+      year: 'numeric', month: 'short', day: 'numeric'
+    },
+    long: {
+      year: 'numeric', month: 'short', day: 'numeric',
+      weekday: 'short', hour: 'numeric', minute: 'numeric'
+    }
+  },
+  'cn': {
+    short: {
+      year: 'numeric', month: 'short', day: 'numeric'
+    },
+    long: {
+      year: 'numeric', month: 'short', day: 'numeric',
+      weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true
+    }
+  }
+}
