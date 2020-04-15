41cf8a6ffb33e9b8e86a8ceac548215f227e67e8
jym503558564 <503558564@qq.com>
2020-04-15 16:37:06 +0800

    [Update] 去掉树

--- src/views/jobcenter/CommandExecution.vue
+++ src/views/jobcenter/CommandExecution.vue
@@ -4,7 +4,8 @@
     <el-collapse-transition>
       <el-row>
         <el-col v-show="ShowTree" :span="4" class="transition-box">
-          <TreeNode :url="treeurl" @urlChanged="handleUrlChange" />
+          <!--          <TreeNode :url="treeurl" @urlChanged="handleUrlChange" />-->
+          这里放资产树
         </el-col>
         <el-col :span="1" class="mini"><div style="display:block" class="mini-button" @click="ShowTree=!ShowTree"><i v-show="ShowTree" id="toggle-icon" class="fa fa-angle-left fa-x" /><i v-show="!ShowTree" id="toggle-icon" class="fa fa-angle-right fa-x" /></div></el-col>
         <el-col :span="ShowTree?19:23" class="transition-box">
@@ -17,11 +18,11 @@
 
 <script>
 import { Page } from '@/layout/components'
-import TreeNode from '@/components/TreeNode'
+// import TreeNode from '@/components/TreeNode'
 export default {
   components: {
-    Page,
-    TreeNode
+    Page
+    // TreeNode
   },
   data() {
     return {
