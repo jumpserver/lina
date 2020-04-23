6ed5123d21a0c3751d02963a96ce553e551d7e08
ibuler <ibuler@qq.com>
2020-04-23 20:50:09 +0800

    [Update] remove click

--- src/components/ListTable/TableAction.vue
+++ src/components/ListTable/TableAction.vue
@@ -7,7 +7,7 @@
       <div class="table-action-right-side">
         <!--        <el-input v-if="hasSearch" v-model="keyword" suffix-icon="el-icon-search" :placeholder="$tc('Search')" class="right-side-item action-search" size="small" clearable @change="handleSearch" @input="handleSearch" />-->
         <TagSearch v-if="hasSearch" class="right-side-item action-search" :tag-search="tagSearch" @tagSearch="handleTagSearch" />
-        <ActionsGroup :is-fa="true" :actions="defaultRightSideActions" class="right-side-actions right-side-item" @actionClick="handleActionClick" />
+        <ActionsGroup :is-fa="true" :actions="defaultRightSideActions" class="right-side-actions right-side-item" />
       </div>
     </slot>
   </div>
