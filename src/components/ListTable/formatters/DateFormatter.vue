09c2f2e349c19b23da03567b157d9071c80b4863
xinwen <coderWen@126.com>
2020-05-08 21:03:10 +0800

    [Add] DateFormatter

---
+++ src/components/ListTable/formatters/DateFormatter.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>{{ value }}</span>
+</template>
+
+<script>
+import BaseFormatter from './base'
+export default {
+  name: 'DateFormatter',
+  extends: BaseFormatter,
+  data() {
+    const dt = new Date(this.cellValue)
+    const year = dt.getFullYear()
+    const month = dt.getMonth() + 1
+    const date = dt.getDate()
+    const hour = dt.getHours()
+    const minutes = dt.getMinutes()
+    const seconds = dt.getSeconds()
+    return {
+      value: `${year}-${month}-${date} ${hour}:${minutes}:${seconds}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
