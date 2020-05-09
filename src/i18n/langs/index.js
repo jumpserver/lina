54e009b62ca4bd63a976e1039cb5c6dbd085f859
Orange <orangemtony@gmail.com>
2020-05-09 14:24:38 +0800

    [update] 修改翻译格式

--- src/i18n/langs/index.js
+++ src/i18n/langs/index.js
@@ -1,5 +1,16 @@
-import en from './en'
-import cn from './cn'
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import enLocale from 'element-ui/lib/locale/lang/en'
+import customCN from './cn.json'
+import customEN from './en.json'
+
+const cn = {
+  ...customCN,
+  ...zhLocale
+}
+const en = {
+  ...customEN,
+  ...enLocale
+}
 export default {
   en,
   cn
