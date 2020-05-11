a516c0711965fabbffc06e74e9f99664c2e17aa2
xinwen <coderWen@126.com>
2020-05-11 15:44:58 +0800

    [Add] 添加 applications DatabaseApp

--- src/views/applications/index.js
+++ src/views/applications/index.js
@@ -2,19 +2,41 @@ export default [
   {
     path: 'remote-apps',
     name: 'RemoteAppList',
-    component: () => import('@/views/applications/RemoteAppList'),
+    component: () => import('@/views/applications/RemoteApp/RemoteAppList'),
     meta: { title: 'RemoteApp' }
+  },
+  {
+    path: 'remote-apps/create',
+    name: 'RemoteAppCreate',
+    component: () => import('@/views/applications/RemoteApp/RemoteAppCreateUpdate'),
+    meta: { title: 'RemoteApp' },
+    hidden: true
   },
   {
     path: 'remote-apps/:id',
     name: 'RemoteAppDetail',
-    component: () => import('@/views/applications/RemoteAppList'),
-    meta: { title: 'RemoteApp' }
+    component: () => import('@/views/applications/RemoteApp/RemoteAppList'),
+    meta: { title: 'RemoteApp' },
+    hidden: true
   },
   {
     path: 'database-apps',
-    name: 'DatabaseAppList',
-    component: () => import('@/views/applications/DatabaseAppList'),
+    name: 'DatabaseApp',
+    component: () => import('@/views/applications/DatabaseApp/DatabaseAppList'),
     meta: { title: 'DatabaseApp' }
+  },
+  {
+    path: 'database-apps/create',
+    name: 'DatabaseAppCreate',
+    component: () => import('@/views/applications/DatabaseApp/DatabaseAppCreateUpdate'),
+    meta: { title: 'DatabaseAppCreate', activeMenu: '/applications/DatabaseApp', action: 'create' },
+    hidden: true
+  },
+  {
+    path: 'database-apps/:id/update',
+    name: 'DatabaseAppUpdate',
+    component: () => import('@/views/applications/DatabaseApp/DatabaseAppCreateUpdate'),
+    meta: { title: 'DatabaseAppUpdate', activeMenu: '/applications/DatabaseApp', action: 'update' },
+    hidden: true
   }
 ]
