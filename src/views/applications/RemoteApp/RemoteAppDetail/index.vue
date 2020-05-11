d07d91c67c06c4bf5cc1eed74aeaca12742a5607
jym503558564 <503558564@qq.com>
2020-05-11 20:13:17 +0800

    [Update] 增加远程应用详情组件

---
+++ src/views/applications/RemoteApp/RemoteAppDetail/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <GenericDetailPage :object.sync="RemoteApp" :active-menu.sync="config.activeMenu" v-bind="config" v-on="$listeners">
+    <keep-alive>
+      <component :is="config.activeMenu" :object="RemoteApp" />
+    </keep-alive>
+  </GenericDetailPage>
+</template>
+
+<script>
+import { GenericDetailPage, TabPage } from '@/layout/components'
+import RemoteAppDetail from './RemoteAppDetail'
+
+export default {
+  components: {
+    GenericDetailPage,
+    RemoteAppDetail,
+    TabPage
+  },
+  data() {
+    return {
+      RemoteApp: {
+        name: '', asset: '', get_type_display: '', path: '', date_created: '', created_by: '', comment: ''
+      },
+      config: {
+        activeMenu: 'RemoteAppDetail',
+        submenu: [
+          {
+            title: this.$t('route.RemoteAppDetail'),
+            name: 'RemoteAppDetail'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/views/applications/index.js
+++ src/views/applications/index.js
@@ -9,14 +9,21 @@ export default [
     path: 'remote-apps/create',
     name: 'RemoteAppCreate',
     component: () => import('@/views/applications/RemoteApp/RemoteAppCreateUpdate'),
-    meta: { title: 'RemoteApp' },
+    meta: { title: 'RemoteApp', activeMenu: '/applications/remote-apps', action: 'create' },
+    hidden: true
+  },
+  {
+    path: 'remote-apps/:id/update',
+    name: 'RemoteAppUpdate',
+    component: () => import('@/views/applications/RemoteApp/RemoteAppCreateUpdate'),
+    meta: { title: 'RemoteAppUpdate', activeMenu: '/applications/remote-apps', action: 'update' },
     hidden: true
   },
   {
     path: 'remote-apps/:id',
     name: 'RemoteAppDetail',
-    component: () => import('@/views/applications/RemoteApp/RemoteAppList'),
-    meta: { title: 'RemoteApp' },
+    component: () => import('@/views/applications/RemoteApp/RemoteAppDetail/index'),
+    meta: { title: 'RemoteAppDetail', activeMenu: '/applications/remote-apps' },
     hidden: true
   },
   {
@@ -43,7 +50,7 @@ export default [
     path: 'database-apps/:id',
     name: 'DatabaseAppDetail',
     component: () => import('@/views/applications/DatabaseApp/DatabaseAppDetail/index'),
-    meta: { title: 'DatabaseAppDetail', activeMenu: '/applications/database-apps', action: 'update' },
+    meta: { title: 'DatabaseAppDetail', activeMenu: '/applications/database-apps' },
     hidden: true
   }
 ]
