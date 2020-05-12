cc564868454865abbcf16c17336c196ba8c62be5
Eric <xplzv@126.com>
2020-05-12 13:00:11 +0800

    ticket list

---
+++ src/views/tickets/TicketDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <GenericDetailPage />
+</template>
+
+<script>
+import { GenericDetailPage } from '@/layout/components'
+export default {
+  name: 'TicketDetail',
+  components: {
+    GenericDetailPage
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/tickets/index.js
+++ src/views/tickets/index.js
@@ -2,7 +2,13 @@ export default [
   {
     path: 'tickets',
     name: 'TicketsList',
-    component: () => import('@/views/tickets/TicketsList'),
+    component: () => import('@/views/tickets/TicketList'),
     meta: { title: 'Tickets', icon: 'check-square-o' }
+  },
+  {
+    path: 'tickets/:id',
+    name: 'TicketDetail',
+    component: () => import('@/views/tickets/TicketDetail'),
+    meta: { title: 'TicketDetail', icon: 'check-square-o' }
   }
 ]
