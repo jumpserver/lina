df4538f78259e8dccab82b36d5c49363a4e103a8
xinwen <coderWen@126.com>
2020-05-12 16:27:29 +0800

    [Add] 创建 applications RemoteApp

--- src/views/applications/RemoteApp/RemoteAppCreateUpdate.vue
+++ src/views/applications/RemoteApp/RemoteAppCreateUpdate.vue
@@ -4,15 +4,39 @@
 
 <script>
 import { GenericCreateUpdatePage } from '@/layout/components'
+import { REMOTE_APP_TYPE_FIELDS_MAP, REMOTE_APP_TYPE_META_MAP } from './const'
+
 export default {
   components: {
     GenericCreateUpdatePage
   },
   data() {
+    const app_type = this.$route.query.type
+
+    const fields_map = REMOTE_APP_TYPE_FIELDS_MAP[app_type]
+    const specific_fields = Object.keys(fields_map)
+
+    const app_type_meta = REMOTE_APP_TYPE_META_MAP[app_type]
+
     return {
       fields: [
+        [this.$t('route.RemoteApp'), ['name', 'asset', 'type', 'path', 'comment']],
+        [this.$t('applications.' + app_type), [...specific_fields]]
       ],
-      url: '/api/v1/applications/remote-apps/'
+      url: '/api/v1/applications/remote-apps/',
+      fieldsMeta: {
+        type: {
+          type: 'select',
+          options: [
+            {
+              label: app_type_meta.title,
+              value: app_type_meta.value
+            }
+          ],
+          disabled: true
+        },
+        ...fields_map
+      }
     }
   }
 }
---
+++ src/views/applications/RemoteApp/const.js
@@ -0,0 +1,43 @@
+import i18n from '@/i18n/i18n'
+
+export const CHROME = 'chrome'
+export const MYSQL_WORKBENCH = 'mysql_workbench'
+export const VMWARE_CLIENT = 'vmware_client'
+export const CUSTOM = 'custom'
+
+export const ALL_TYPES = [CHROME, MYSQL_WORKBENCH, VMWARE_CLIENT, CUSTOM]
+
+function gen_type_fields_config(fields) {
+  const config = {}
+  fields.forEach((item, index, array) => {
+    config[item] = {
+      prop: 'params.' + item,
+      label: i18n.t('applications.' + item)
+    }
+  })
+  return config
+}
+
+export const REMOTE_APP_TYPE_FIELDS_MAP = {}
+
+REMOTE_APP_TYPE_FIELDS_MAP[CHROME] = gen_type_fields_config([
+  'chrome_target', 'chrome_username', 'chrome_password'
+])
+REMOTE_APP_TYPE_FIELDS_MAP[MYSQL_WORKBENCH] = gen_type_fields_config([
+  'mysql_workbench_ip', 'mysql_workbench_name', 'mysql_workbench_username', 'mysql_workbench_password'
+])
+REMOTE_APP_TYPE_FIELDS_MAP[VMWARE_CLIENT] = gen_type_fields_config([
+  'vmware_target', 'vmware_username', 'vmware_password'
+])
+REMOTE_APP_TYPE_FIELDS_MAP[CUSTOM] = gen_type_fields_config([
+  'custom_cmdline', 'custom_target', 'custom_username', 'custom_password'
+])
+
+export const REMOTE_APP_TYPE_META_MAP = {}
+
+for (const value of ALL_TYPES) {
+  REMOTE_APP_TYPE_META_MAP[value] = {
+    name: value,
+    title: i18n.t('applications.' + value)
+  }
+}
