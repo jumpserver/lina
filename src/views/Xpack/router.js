3cecfcac7982769d7624f680a9a93838e5a5eca8
OrangeM21 <orangemtony@gmail.com>
2020-05-13 20:46:36 +0800

    [fix]

--- src/views/Xpack/router.js
+++
@@ -1,8 +0,0 @@
-export default {
-  path: '/Xpack',
-  component: () => import('@/views/jobcenter/CeleryTaskLog'),
-  redirect: '/applications/remote-apps/',
-  name: 'Xpack',
-  meta: { title: 'Xpack', icon: 'th' }
-}
-
