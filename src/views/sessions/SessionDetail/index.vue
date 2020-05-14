b84454c503b497458d7a00cc430a031da2f76257
Eric_Lee <xplzv@126.com>
2020-05-14 19:34:38 +0800

    session detail view (#52)

---
+++ src/views/sessions/SessionDetail/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <GenericDetailPage
+    :submenu="submenu"
+    :object.sync="sessionData"
+    :active-menu.sync="activeSubMenu"
+    :has-right-side="false"
+    :get-object-name="getSessionName"
+  >
+    <keep-alive>
+      <component :is="activeSubMenu" :object="sessionData" />
+    </keep-alive>
+  </GenericDetailPage>
+</template>
+
+<script>
+import { GenericDetailPage } from '@/layout/components'
+import SessionCommandList from './SessionCommandList'
+import SessionDetailCard from './SessionDetailCard'
+export default {
+  name: 'SessionDetail',
+  components: {
+    GenericDetailPage,
+    SessionCommandList,
+    SessionDetailCard
+  },
+  data() {
+    return {
+      sessionData: {},
+      activeSubMenu: 'SessionDetailCard',
+      submenu: [
+        {
+          title: this.$t('route.SessionDetail'),
+          name: 'SessionDetailCard'
+        },
+        {
+          title: this.$t('sessions.command'),
+          name: 'SessionCommandList'
+        }
+      ]
+    }
+  },
+  methods: {
+    getSessionName() {
+      return this.sessionData.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
