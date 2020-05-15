f8fb2ede10f5757aa1ad7b1ce440f8cdad028743
ibuler <ibuler@qq.com>
2020-05-15 13:30:42 +0800

    [Update] 修改导入导出

--- src/components/ListTable/TableAction/index.vue
+++ src/components/ListTable/TableAction/index.vue
@@ -2,15 +2,14 @@
   <div class="table-header">
     <slot name="header">
       <div class="table-header-left-side">
-        <LeftSide v-if="hasLeftActions" :selected-rows="selectedRows" v-bind="$attrs" v-on="$listeners" />
+        <LeftSide v-if="hasLeftActions" :selected-rows="selectedRows" :table-url="tableUrl" v-bind="$attrs" v-on="$listeners" />
         <span v-else>
           <AutoDataSearch v-if="hasSearch" class="right-side-item action-search" :config="searchConfig" :url="tableUrl" @tagSearch="handleTagSearch" />
         </span>
       </div>
       <div class="table-action-right-side">
         <AutoDataSearch v-if="hasLeftActions && hasSearch" class="right-side-item action-search" :config="searchConfig" :url="tableUrl" @tagSearch="handleTagSearch" />
-        <ActionsGroup :is-fa="true" :actions="rightSideActions" class="right-side-actions right-side-item" />
-        <DialogAction :selected-rows="selectedRows" :url="tableUrl" />
+        <RightSide v-if="hasRightActions" :selected-rows="selectedRows" :table-url="tableUrl" v-bind="$attrs" v-on="$listeners" />
       </div>
     </slot>
   </div>
@@ -18,30 +17,22 @@
 
 <script>
 import AutoDataSearch from '@/components/AutoDataSearch'
-import DialogAction from '@/components/DialogAction'
-import ActionsGroup from '@/components/ActionsGroup'
 import LeftSide from './LeftSide'
-import { cleanActions } from './utils'
-import _ from 'lodash'
+import RightSide from './RightSide'
 
 const defaultTrue = { type: Boolean, default: true }
-const defaultFalse = { type: Boolean, default: false }
 
 export default {
   name: 'TableAction',
   components: {
-    ActionsGroup,
-    DialogAction,
     AutoDataSearch,
-    LeftSide
+    LeftSide,
+    RightSide
   },
   props: {
-    hasExport: defaultTrue,
-    hasImport: defaultTrue,
-    hasRefresh: defaultTrue,
     hasLeftActions: defaultTrue,
     hasSearch: defaultTrue,
-    hasRightActions: defaultFalse,
+    hasRightActions: defaultTrue,
     searchConfig: {
       type: Object,
       default: () => ({})
@@ -52,7 +43,7 @@ export default {
     },
     searchTable: {
       type: Function,
-      default: () => {}
+      default: (val) => {}
     },
     selectedRows: {
       type: Array,
@@ -63,52 +54,21 @@ export default {
       default() {
         return []
       }
-    },
-    extraRightSideActions: {
-      type: Array,
-      default: () => []
     }
   },
   data() {
     return {
-      keyword: '',
-      defaultRightSideActions: [
-        { name: 'actionExport', fa: 'fa-download', has: this.hasExport, callback: this.handleExport },
-        { name: 'actionImport', fa: 'fa-upload', has: this.hasImport, callback: this.handleImport },
-        { name: 'actionRefresh', fa: 'fa-refresh', has: this.hasRefresh, callback: this.handleRefresh }
-      ],
-      dialogExportVisible: false,
-      exportValue: 2
+      keyword: ''
     }
   },
   computed: {
-    rightSideActions() {
-      const actions = [...this.defaultRightSideActions, ...this.extraRightSideActions]
-      const params = {
-        selectedRows: this.selectedRows,
-        reloadTable: this.reloadTable
-      }
-      return cleanActions(actions, true, params)
-    },
     hasSelectedRows() {
       return this.selectedRows.length > 0
     }
   },
   methods: {
-    handleSearch: _.debounce(function() {
-      this.searchTable({ search: this.keyword })
-    }, 500),
     handleTagSearch(val) {
       this.searchTable(val)
-    },
-    handleExport({ selectedRows }) {
-      this.$eventBus.$emit('showExportDialog', { selectedRows })
-    },
-    handleImport({ selectedRows }) {
-      this.$eventBus.$emit('showImportDialog', { selectedRows })
-    },
-    handleRefresh() {
-      this.reloadTable()
     }
   }
 }
