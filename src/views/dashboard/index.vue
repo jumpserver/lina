ad18663a39c6ef264e20ec9f3f537055874b880e
Bai <bugatti_it@163.com>
2020-05-18 12:02:19 +0800

    [Update] Dashboard (2)

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,61 +1,42 @@
 <template>
   <Page>
     <el-row :gutter="20">
-      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
+      <el-col :span="6"><SummaryCard /></el-col>
+      <el-col :span="6"><SummaryCard /></el-col>
+      <el-col :span="6"><SummaryCard /></el-col>
+      <el-col :span="6"><SummaryCard /></el-col>
     </el-row>
     <el-row :gutter="20">
-      <el-col :span="4"><div class="grid-content bg-purple" /></el-col>
-      <el-col :span="16"><div class="grid-content bg-purple" /></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple" /></el-col>
+      <el-col :span="4"><SummaryCard /></el-col>
+      <el-col :span="16"><SummaryCard /></el-col>
+      <el-col :span="4"><SummaryCard /></el-col>
     </el-row>
-    <el-row type="flex" class="row-bg" justify="space-between">
-      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple-light" /></el-col>
-      <el-col :span="6"><div class="grid-content bg-purple" /></el-col>
+    <el-row :gutter="20">
+      <el-col :span="8"><SummaryCard /></el-col>
+      <el-col :span="8"><SummaryCard /></el-col>
+      <el-col :span="8"><SummaryCard /></el-col>
     </el-row>
   </Page>
 </template>
 
 <script>
 import { Page } from '@/layout/components'
+import { SummaryCard } from '@/components'
 
 export default {
   name: 'Dashboard',
   components: {
-    Page
+    Page, SummaryCard
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
   .el-row {
-    margin-bottom: 20px;
-    min-height: 10px;
+    margin-bottom: 25px;
     &:last-child {
       margin-bottom: 0;
     }
   }
-  .el-col {
-    border-radius: 4px;
-    background-color: #000;
-  }
-  .bg-purple {
-    background: #000;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .bg-purple-light {
-    background: #000;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #000;
-  }
-}
+
 </style>
