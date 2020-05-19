2bb61ff7260f73bcd63455a27da9463c44ba787d
OrangeM21 <orangemtony@gmail.com>
2020-05-19 20:57:56 +0800

    [upate]更新用户界面

--- src/userviews/users/index.vue
+++ src/userviews/users/index.vue
@@ -1,17 +1,38 @@
 <template>
-    <div/>
+  <GenericDetailPage :object.sync="TaskDetail" :active-menu.sync="config.activeMenu" v-bind="config" v-on="$listeners">
+    <keep-alive>
+      <component :is="config.activeMenu" :object="TaskDetail" />
+    </keep-alive>
+  </GenericDetailPage>
 </template>
 
 <script>
+  import { GenericDetailPage } from '@/layout/components'
+  import Detail from './Detail.vue'
   export default {
-    name: '',
-    components: {},
+    name: 'AssetListDetail',
+    components: {
+      GenericDetailPage,
+      Detail
+    },
     data() {
-      return {}
+      return {
+        TaskDetail: {},
+        config: {
+          title: this.$t('assets.SystemUserDetail'),
+          activeMenu: 'Detail',
+          submenu: [
+            {
+              title: this.$t('assets.detail'),
+              name: 'Detail'
+            }
+          ],
+          hasRightSide: false
+        }
+      }
     }
   }
 </script>
 
-<style lang='less' scoped>
-
+<style scoped>
 </style>
