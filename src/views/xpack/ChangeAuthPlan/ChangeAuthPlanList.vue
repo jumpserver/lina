a4c39a6d92fbead7e180db095da90c4294a94f5d
Bai <bugatti_it@163.com>
2020-05-21 19:56:07 +0800

    [Update] 添加改密计划列表（1）

--- src/views/xpack/ChangeAuthPlan/ChangeAuthPlanList.vue
+++ src/views/xpack/ChangeAuthPlan/ChangeAuthPlanList.vue
@@ -1,10 +1,42 @@
 <template>
-  <h1>change auth plan list</h1>
+  <GenericListPage :table-config="tableConfig" :header-actions="headerActions" />
 </template>
 
 <script>
+import { GenericListPage } from '@/layout/components'
+
 export default {
-  name: 'ChangeAuthPlan'
+  name: 'ChangeAuthPlan',
+  components: {
+    GenericListPage
+  },
+  data() {
+    const vm = this
+    return {
+      tableConfig: {
+        url: '/api/v1/xpack/change-auth-plan/plan/',
+        columns: [
+          'name', 'username', 'assets_amount', 'nodes_amount', 'password_strategy_display', 'periodic_display', 'run_times',
+          'comment', 'actions'
+        ],
+        columnsMeta: {
+          password_strategy_display: {
+            width: '200px',
+            showOverflowTooltip: true
+          },
+          assets_amount: {
+            label: vm.$t('xpack.Asset')
+          },
+          nodes_amount: {
+            label: vm.$t('xpack.Node')
+          }
+        }
+      },
+      headerActions: {
+
+      }
+    }
+  }
 }
 </script>
 
