17366886df47b72197a2c4b921c1d1dfdfaaca9d
jym503558564 <503558564@qq.com>
2020-05-21 15:40:02 +0800

    [Update] 组织列表页面只显示名称和备注，删掉多余的fields

--- src/views/xpack/org/OrganizationList.vue
+++ src/views/xpack/org/OrganizationList.vue
@@ -4,7 +4,6 @@
 
 <script>
 import { GenericListPage } from '@/layout/components'
-import OrgFieldsFormatter from './OrgFieldsFormatter'
 
 export default {
   components: {
@@ -14,53 +13,7 @@ export default {
     return {
       tableConfig: {
         url: '/api/v1/orgs/orgs/',
-        columns: [
-          'name', 'admins', 'auditors', 'users', 'user_groups', 'assets', 'domains',
-          'admin_users', 'system_users', 'labels', 'perms', 'comment', 'actions'
-        ],
-        columnsMeta: {
-          admins: {
-            label: this.$t('xpack.Admin'),
-            formatter: OrgFieldsFormatter
-          },
-          auditors: {
-            label: this.$t('xpack.Auditor'),
-            formatter: OrgFieldsFormatter
-          },
-          users: {
-            label: this.$t('xpack.User'),
-            formatter: OrgFieldsFormatter
-          },
-          user_groups: {
-            label: this.$t('xpack.UserGroup'),
-            formatter: OrgFieldsFormatter
-          },
-          assets: {
-            label: this.$t('xpack.Asset'),
-            formatter: OrgFieldsFormatter
-          },
-          domains: {
-            label: this.$t('xpack.Domain'),
-            formatter: OrgFieldsFormatter
-          },
-          admin_users: {
-            label: this.$t('xpack.AdminUser'),
-            formatter: OrgFieldsFormatter
-          },
-          system_users: {
-            label: this.$t('xpack.SystemUser'),
-            formatter: OrgFieldsFormatter
-          },
-          labels: {
-            label: this.$t('xpack.Admin'),
-            formatter: OrgFieldsFormatter
-          },
-          perms: {
-            label: this.$t('xpack.Label'),
-            formatter: OrgFieldsFormatter
-          }
-        },
-        detailRoute: 'OrganizationDetail'
+        columns: ['name', 'comment', 'actions']
       },
       headerActions: {
         hasExport: false,
