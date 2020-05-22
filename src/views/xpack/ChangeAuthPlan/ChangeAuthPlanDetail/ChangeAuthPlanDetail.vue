7184f0c02b9049a1e4fac826218f785bdd2f55c3
Bai <bugatti_it@163.com>
2020-05-22 19:07:02 +0800

    [Update] 添加改密计划详情页面翻译（1）

--- src/views/xpack/ChangeAuthPlan/ChangeAuthPlanDetail/ChangeAuthPlanDetail.vue
+++ src/views/xpack/ChangeAuthPlan/ChangeAuthPlanDetail/ChangeAuthPlanDetail.vue
@@ -1,10 +1,54 @@
 <template>
-  <div>change auth plan detail</div>
+  <el-row :gutter="20">
+    <el-col :md="14" :sm="24">
+      <DetailCard :items="detailItems" />
+    </el-col>
+    <el-col :md="10" :sm="24">
+      <QuickActions :actions="quickActions" type="primary" />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import { DetailCard, QuickActions } from '@/components'
+
 export default {
-  name: 'ChangeAuthPlanDetail'
+  name: 'ChangeAuthPlanInfo',
+  components: {
+    DetailCard,
+    QuickActions
+  },
+  props: {
+    object: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      quickActions: [
+
+      ]
+    }
+  },
+  computed: {
+    detailItems() {
+      return [
+        {
+          key: 'name',
+          value: this.object.name
+        },
+        {
+          key: 'username',
+          value: this.object.username
+        },
+        {
+          key: 'password strategy',
+          value: this.object.password_strategy
+        }
+      ]
+    }
+  }
 }
 </script>
 
--- src/views/xpack/ChangeAuthPlan/ChangeAuthPlanDetail/index.vue
+++ src/views/xpack/ChangeAuthPlan/ChangeAuthPlanDetail/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>change auth plan detail</div>
+  <GenericDetailPage :object.sync="plan" :active-menu.sync="config.activeMenu" v-bind="config">
+    <keep-alive>
+      <component :is="config.activeMenu" :object="plan" />
+    </keep-alive>
+  </GenericDetailPage>
 </template>
 
 <script>
+import { GenericDetailPage } from '@/layout/components'
+import ChangeAuthPlanInfo from './ChangeAuthPlanDetail'
+
 export default {
+  components: {
+    GenericDetailPage,
+    ChangeAuthPlanInfo
+  },
+  data() {
+    return {
+      plan: { name: '', username: '', comment: '' },
+      config: {
+        activeMenu: 'ChangeAuthPlanInfo',
+        submenu: [
+          {
+            title: this.$t('common.BasicInfo'),
+            name: 'ChangeAuthPlanInfo'
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
