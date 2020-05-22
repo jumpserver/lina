b5ad85ba38ed5164878139b38ee1d3879ac86a51
Eric <xplzv@126.com>
2020-05-22 11:17:26 +0800

    update session list

--- src/views/sessions/SessionList/index.vue
+++ src/views/sessions/SessionList/index.vue
@@ -23,7 +23,7 @@ export default {
   data() {
     return {
       config: {
-        activeMenu: 'SessionOnline',
+        activeMenu: 'OfflineList',
         submenu: [
           {
             title: this.$t('route.SessionOnline'),
