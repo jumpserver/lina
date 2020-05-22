2eb6454313dff0dde9d6a209826bcb9d377a3e10
Bai <bugatti_it@163.com>
2020-05-22 17:58:56 +0800

    [Update] 添加改密计划创建/更新页面翻译（4）

--- src/views/xpack/ChangeAuthPlan/ChangeAuthPlanCreateUpdate.vue
+++ src/views/xpack/ChangeAuthPlan/ChangeAuthPlanCreateUpdate.vue
@@ -28,6 +28,9 @@ export default {
         interval: 24
       },
       fieldsMeta: {
+        username: {
+          helpText: '用户名为将要修改的资产上的用户的用户名。如果用户存在，则修改密码；如果用户不存在，则创建用户。'
+        },
         assets: {
           type: 'assetSelect',
           component: AssetSelect,
@@ -72,13 +75,22 @@ export default {
           label: this.$t('xpack.RegularlyPerform'),
           hidden: (formValue) => {
             return formValue.is_periodic === false
-          }
+          },
+          helpText: '' +
+            'eg：每周日 03:05 执行 <5 3 * * 0>' +
+            '<br/>' +
+            '使用5位 Linux crontab 表达式 <分 时 日 月 星期> ' +
+            '（<a href="https://tool.lu/crontab/" target="_blank">在线工具</a>）' +
+            '<br/>' +
+            '如果同时设置了定期执行和周期执行，优先使用定期执行' +
+            ''
         },
         interval: {
           label: this.$t('xpack.CyclePerform'),
           hidden: (formValue) => {
             return formValue.is_periodic === false
-          }
+          },
+          helpText: '（单位： 时）'
         }
       },
       cleanFormValue(values) {
