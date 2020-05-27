d7447b43cb8ad4238ba94e2ba0a1ca0111071041
Bai <bugatti_it@163.com>
2020-05-27 16:59:52 +0800

    [Update] 添加UserProfile页面；Nav 添加 userProfile 跳转；routes 添加commonRoutes（userProfile)

--- src/userviews/users/index.vue
+++ src/userviews/users/index.vue
@@ -1,37 +1,36 @@
 <template>
-  <GenericDetailPage :object.sync="TaskDetail" :active-menu.sync="config.activeMenu" v-bind="config" v-on="$listeners">
+  <GenericDetailPage :object.sync="user" :active-menu.sync="config.activeMenu" v-bind="config" v-on="$listeners">
     <keep-alive>
-      <component :is="config.activeMenu" :object="TaskDetail" />
+      <component :is="config.activeMenu" :object="user" />
     </keep-alive>
   </GenericDetailPage>
 </template>
 
 <script>
-  import { GenericDetailPage } from '@/layout/components'
-  import Detail from './Detail.vue'
-  export default {
-    name: 'AssetListDetail',
-    components: {
-      GenericDetailPage,
-      Detail
-    },
-    data() {
-      return {
-        TaskDetail: {},
-        config: {
-          title: this.$t('assets.SystemUserDetail'),
-          activeMenu: 'Detail',
-          submenu: [
-            {
-              title: this.$t('assets.detail'),
-              name: 'Detail'
-            }
-          ],
-          hasRightSide: false
-        }
+import { GenericDetailPage } from '@/layout/components'
+import UserProfile from './UserProfile.vue'
+export default {
+  name: 'AssetListDetail',
+  components: {
+    GenericDetailPage,
+    UserProfile
+  },
+  data() {
+    return {
+      user: {},
+      config: {
+        title: this.$t('common.nav.Profile'),
+        submenu: [
+          {
+            title: this.$t('common.BasicInfo'),
+            name: 'UserProfile'
+          }
+        ],
+        hasRightSide: false
       }
     }
   }
+}
 </script>
 
 <style scoped>
