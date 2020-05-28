c84f8dd48c58b7c382ca6dccd694a9eaa65d6069
Bai <bugatti_it@163.com>
2020-05-28 16:10:55 +0800

    [Update] 更新个人信息页面（登录密码设置）

--- src/userviews/users/UserProfile/PasswordUpdate.vue
+++ src/userviews/users/UserProfile/PasswordUpdate.vue
@@ -1,10 +1,62 @@
 <template>
-  <div>password update</div>
+  <IBox>
+    <GenericCreateUpdateForm
+      :fields="fields"
+      :fields-meta="fieldsMeta"
+      :initial="object"
+      :url="url"
+      :get-method="getMethod"
+    />
+  </IBox>
 </template>
 
 <script>
+import GenericCreateUpdateForm from '@/layout/components/GenericCreateUpdateForm'
+import { IBox } from '@/components'
+
 export default {
-  name: 'PasswordUpdate'
+  name: 'PasswordUpdate',
+  components: {
+    GenericCreateUpdateForm,
+    IBox
+  },
+  props: {
+    object: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      url: '/api/v1/users/profile/password/',
+      fields: ['old_password', 'new_password', 'new_password_again'],
+      fieldsMeta: {
+        old_password: {
+          label: '原密码',
+          el: {
+            type: 'password'
+          }
+        },
+        new_password: {
+          label: '新密码',
+          el: {
+            type: 'password'
+          }
+        },
+        new_password_again: {
+          label: '确认密码',
+          el: {
+            type: 'password'
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    getMethod() {
+      return 'put'
+    }
+  }
 }
 </script>
 
