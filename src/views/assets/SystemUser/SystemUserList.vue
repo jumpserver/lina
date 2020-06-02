46bfe8d966a1dfbc5715e0afbf1c5875d18889fa
OrangeM21 <orangemtony@gmail.com>
2020-06-02 15:11:19 +0800

    [fix]修复打开新窗口问题

--- src/views/assets/SystemUser/SystemUserList.vue
+++ src/views/assets/SystemUser/SystemUserList.vue
@@ -64,7 +64,7 @@ export default {
         ]
       },
       headerActions: {
-        // hasBulkDelete: false,
+        hasBulkDelete: false,
         hasMoreActions: false,
         createRoute: 'SystemUserCreate'
       },
