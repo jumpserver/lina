d9016a4dcfa1a0a2559c13f30f1bef953be1bcfe
Bai <bugatti_it@163.com>
2020-06-04 21:02:44 +0800

    [Update] 修改 route 位置，vault / cloud

--- src/views/xpack/router.js
+++ src/views/xpack/router.js
@@ -135,6 +135,26 @@ export default {
       ]
     },
     {
+      path: 'vault',
+      component: empty,
+      meta: { title: i18n.t('xpack.Vault.Vault'), activeMenu: '/xpack/vault/vault' },
+      children: [
+        {
+          path: 'vault',
+          component: () => import('@/views/xpack/Vault/VaultList.vue'),
+          name: 'VaultList',
+          meta: { title: i18n.t('xpack.Vault.Vault'), activeMenu: '/xpack/vault/vault' }
+        },
+        {
+          path: 'vault/create',
+          component: () => import('@/views/xpack/Vault/VaultCreate'),
+          name: 'VaultCreate',
+          meta: { title: i18n.t('xpack.Vault.Create'), activeMenu: '/xpack/vault/vault' },
+          hidden: true
+        }
+      ]
+    },
+    {
       path: '/cloud',
       component: empty,
       redirect: '/cloud/account',
@@ -180,26 +200,6 @@ export default {
           name: 'SyncInstanceTaskUpdate',
           hidden: true,
           meta: { title: i18n.t('xpack.Cloud.SyncInstanceTaskUpdate') }
-        }
-      ]
-    },
-    {
-      path: 'vault',
-      component: empty,
-      meta: { title: i18n.t('xpack.Vault.Vault'), activeMenu: '/xpack/vault/vault' },
-      children: [
-        {
-          path: 'vault',
-          component: () => import('@/views/xpack/Vault/VaultList.vue'),
-          name: 'VaultList',
-          meta: { title: i18n.t('xpack.Vault.Vault'), activeMenu: '/xpack/vault/vault' }
-        },
-        {
-          path: 'vault/create',
-          component: () => import('@/views/xpack/Vault/VaultCreate'),
-          name: 'VaultCreate',
-          meta: { title: i18n.t('xpack.Vault.Create'), activeMenu: '/xpack/vault/vault' },
-          hidden: true
         }
       ]
     }
