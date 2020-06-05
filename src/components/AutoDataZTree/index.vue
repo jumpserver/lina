edb1105e9eb0c3a219b6e4ab47b1694c3ee72745
OrangeM21 <orangemtony@gmail.com>
2020-06-05 14:21:42 +0800

    [fix]修复引用Ztree问题

--- src/components/AutoDataZTree/index.vue
+++ src/components/AutoDataZTree/index.vue
@@ -102,7 +102,6 @@ export default {
       }
     },
     removeTreeNode: function() {
-      const vm = this
       this.hideRMenu()
       const currentNode = this.zTree.getSelectedNodes()[0]
       if (!currentNode) {
@@ -111,14 +110,13 @@ export default {
       this.$axios.delete(
         `${this.treeSetting.nodeUrl}${currentNode.meta.node.id}/`
       ).then(() => {
-        vm.$refs.dataztree.refresh()
         this.$message.success(this.$t('common.deleteSuccessMsg'))
+        this.zTree.removeNode(currentNode)
       }).catch(error => {
         this.$message.error(this.$t('common.deleteErrorMsg' + ' ' + error))
       })
     },
     onRename: function(event, treeId, treeNode, isCancel) {
-      const vm = this
       const url = `${this.treeSetting.nodeUrl}${this.currentNodeId}/`
       if (isCancel) {
         return
@@ -133,7 +131,7 @@ export default {
         }
         treeNode.name = treeNode.name + ' (' + assets_amount + ')'
         this.zTree.updateNode(treeNode)
-        vm.$refs.dataztree.refresh()
+        // vm.$refs.dataztree.refresh()
         this.$message.success(this.$t('common.updateSuccessMsg'))
       }).catch(error => {
         this.$message.error(this.$t('common.updateErrorMsg' + ' ' + error))
@@ -198,7 +196,6 @@ export default {
     },
     addTreeNode: function() {
       this.hideRMenu()
-      const vm = this
       const parentNode = this.zTree.getSelectedNodes()[0]
       if (!parentNode) {
         return
@@ -218,9 +215,10 @@ export default {
         }
         newNode.checked = this.zTree.getSelectedNodes()[0].checked
         this.zTree.addNodes(parentNode, 0, newNode)
-        vm.$refs.dataztree.refresh()
-        // const node = this.zTree.getNodeByParam('id', newNode.id, parentNode)
-        // this.zTree.editName(node)
+        // vm.$refs.dataztree.refresh()
+        const node = this.zTree.getNodeByParam('id', newNode.id, parentNode)
+        this.currentNodeId = node.meta.node.id
+        this.zTree.editName(node)
         this.$message.success(this.$t('common.updateSuccessMsg'))
       }).catch(error => {
         this.$message.error(this.$t('common.updateErrorMsg' + ' ' + error))
