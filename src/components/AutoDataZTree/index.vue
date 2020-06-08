b29a61b79260c2d610527b4502d287bd1993b62c
Bai <bugatti_it@163.com>
2020-06-08 14:02:59 +0800

    [Update] 添加更新节点资产硬件信息执行逻辑

--- src/components/AutoDataZTree/index.vue
+++ src/components/AutoDataZTree/index.vue
@@ -148,7 +148,19 @@ export default {
 
     },
     updateNodeAssetHardwareInfo: function() {
-
+      this.hideRMenu()
+      const currentNode = this.zTree.getSelectedNodes()[0]
+      if (!currentNode) {
+        return
+      }
+      this.$axios.post(
+        `/api/v1/assets/nodes/${currentNode.meta.node.id}/tasks/`,
+        { 'action': 'refresh' }
+      ).then((res) => {
+        window.open(`/core/ops/celery/task/${res.task}/log/`, '_blank', 'toolbar=yes, width=900, height=600')
+      }).catch(error => {
+        this.$message.error(this.$t('common.updateErrorMsg' + ' ' + error))
+      })
     },
     testNodeAssetConnectivity: function() {
 
