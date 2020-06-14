5e0f3783d61e11690e49527c7a10f3edc6dee988
OrangeM21 <orangemtony@gmail.com>
2020-06-14 12:37:57 +0800

    [update]添加企业版链接

--- src/layout/components/NavHeader/Help.vue
+++ src/layout/components/NavHeader/Help.vue
@@ -5,6 +5,7 @@
     </span>
     <el-dropdown-menu slot="dropdown">
       <el-dropdown-item command="docs">{{ $t('common.nav.Docs') }}</el-dropdown-item>
+      <el-dropdown-item command="EnterpriseEdition">{{ $t('common.nav.EnterpriseEdition') }}</el-dropdown-item>
       <el-dropdown-item command="support">{{ $t('common.nav.Support') }}</el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
@@ -18,6 +19,9 @@ export default {
       switch (command) {
         case 'support':
           window.open('http://www.jumpserver.org/support/', '_blank')
+          break
+        case 'EnterpriseEdition':
+          window.open('https://jumpserver.org/enterprise.html', '_blank')
           break
         default:
           window.open('http://docs.jumpserver.org', '_blank')
