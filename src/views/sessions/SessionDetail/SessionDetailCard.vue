64fd5bf1a9ad5033e90d50e9d292b339894fc181
OrangeM21 <orangemtony@gmail.com>
2020-07-15 16:32:26 +0800

    fix(SessionList): 修复命令详情参数及在线会话快捷操作组件
    Closes https://github.com/jumpserver/trello/issues/72

--- src/views/sessions/SessionDetail/SessionDetailCard.vue
+++ src/views/sessions/SessionDetail/SessionDetailCard.vue
@@ -12,6 +12,7 @@
 <script>
 import DetailCard from '@/components/DetailCard/index'
 import QuickActions from '@/components/QuickActions'
+import { terminateSession } from '@/api/sessions'
 export default {
   name: 'SessionDetailCard',
   components: {
@@ -68,33 +69,75 @@ export default {
     },
     quickActions() {
       const vm = this
-      return [
-        {
-          title: this.$t('sessions.replaySession'),
-          attrs: {
-            type: 'primary',
-            label: this.$t('sessions.go')
-          },
-          callbacks: {
-            click: function() {
-              vm.openReplaySession(vm.sessionData.id)
-            }
+      if (vm.sessionData.is_finished) {
+        return [
+          {
+            title: this.$t('sessions.replaySession'),
+            attrs: {
+              type: 'primary',
+              label: this.$t('sessions.go')
+            },
+            callbacks: {
+              click: function() {
+                vm.openReplaySession(vm.sessionData.id)
+              }
 
-          }
-        },
-        {
-          title: this.$t('sessions.downloadReplay'),
-          attrs: {
-            type: 'primary',
-            label: this.$t('sessions.download')
+            }
           },
-          callbacks: {
-            click: function() {
-              vm.openReplayDownload(vm.sessionData.id)
+          {
+            title: this.$t('sessions.downloadReplay'),
+            attrs: {
+              type: 'primary',
+              label: this.$t('sessions.download')
+            },
+            callbacks: {
+              click: function() {
+                vm.openReplayDownload(vm.sessionData.id)
+              }
             }
           }
-        }
-      ]
+        ]
+      } else {
+        return [
+          {
+            title: this.$t('sessions.sessionTerminate'),
+            attrs: {
+              type: 'danger',
+              label: this.$t('sessions.terminate')
+            },
+            callbacks: {
+              click: function() {
+              // 终断 session reload
+                terminateSession(vm.sessionData.id).then(res => {
+                  const msg = vm.$t('sessions.TerminateTaskSendSuccessMsg')
+                  vm.$message.success(msg)
+                  window.setTimeout(function() {
+                    window.location.reload()
+                  }, 50000)
+                }
+                )
+              }
+            }
+          },
+          {
+            title: this.$t('sessions.sessionMonitor'),
+            attrs: {
+              type: 'primary',
+              label: this.$t('sessions.Monitor')
+            },
+            can: () => {
+              return vm.sessionData.can_join
+            },
+            callbacks: {
+              click: function() {
+              // 跳转到luna页面
+                const joinUrl = '/luna/join/?shareroom=' + vm.sessionData.id
+                window.open(joinUrl, 'height=600, width=800, top=400, left=400, toolbar=no, menubar=no, scrollbars=no, location=no, status=no')
+              }
+            }
+          }
+        ]
+      }
     }
   },
   methods: {
