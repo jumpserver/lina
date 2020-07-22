18b2f8b61bc564ad10281cc6aa787a3ba35ab655
OrangeM21 <orangemtony@gmail.com>
2020-07-23 02:37:51 +0800

    feat(terminalStorage): 添加批量更新终端存储功能
    Closes https://github.com/jumpserver/jumpserver/issues/4392
    Closes https://github.com/jumpserver/jumpserver/issues/4172

--- src/views/sessions/TerminalList.vue
+++ src/views/sessions/TerminalList.vue
@@ -1,17 +1,62 @@
 <template>
-  <GenericListPage :table-config="tableConfig" :header-actions="headerActions" />
+  <div>
+    <GenericListPage :table-config="tableConfig" :header-actions="headerActions" />
+    <Dialog
+      :visible.sync="dialogSettings.visible"
+      :destroy-on-close="true"
+      :show-cancel="false"
+      :title="$t('sessions.terminalUpdateStorage')"
+      :show-confirm="false"
+    >
+      <GenericCreateUpdateForm v-bind="dialogSettings.iFormSetting" />
+    </Dialog>
+  </div>
 </template>
 
 <script>
-import { GenericListPage } from '@/layout/components'
+import { GenericListPage, GenericCreateUpdateForm } from '@/layout/components'
+import Dialog from '@/components/Dialog'
+import Select2 from '@/components/Select2'
 import { BooleanFormatter } from '@/components/ListTable/formatters'
 
 export default {
   components: {
-    GenericListPage
+    GenericListPage,
+    Dialog,
+    GenericCreateUpdateForm
   },
   data() {
     return {
+      dialogSettings: {
+        selectedRows: [],
+        visible: false,
+        iFormSetting: {
+          url: '/api/v1/terminal/terminals/',
+          fields: [
+            ['', ['command_storage', 'replay_storage']]
+          ],
+          fieldsMeta: {
+            command_storage: {
+              component: Select2,
+              el: {
+                ajax: {
+                  url: `/api/v1/terminal/command-storages/`
+                },
+                multiple: false
+              }
+            },
+            replay_storage: {
+              component: Select2,
+              el: {
+                ajax: {
+                  url: `/api/v1/terminal/replay-storages/`
+                },
+                multiple: false
+              }
+            }
+          }
+        }
+      },
       tableConfig: {
         url: '/api/v1/terminal/terminals/',
         columns: ['name', 'remote_addr', 'session_online', 'is_active', 'is_alive', 'actions'],
@@ -52,6 +97,17 @@ export default {
             can: true,
             callback: this.handleStorageConfiguration
           }
+        ],
+        extraMoreActions: [
+          {
+            name: 'updateSelected',
+            title: this.$t('common.updateSelected'),
+            can: ({ selectedRows }) => selectedRows.length > 0,
+            callback: function({ selectedRows, reloadTable }) {
+              this.dialogSettings.selectedRows = selectedRows
+              this.dialogSettings.visible = true
+            }.bind(this)
+          }
         ]
       }
     }
@@ -59,6 +115,12 @@ export default {
   methods: {
     handleStorageConfiguration() {
       this.$router.push('/terminal/storages')
+    },
+    onCancel() {
+      this.dialogSettings.visible = false
+    },
+    onConfirm() {
+      this.dialogSettings.visible = false
     }
   }
 }
