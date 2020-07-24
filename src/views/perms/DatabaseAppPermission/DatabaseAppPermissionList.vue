d0143ab0fe9b58a0e84e4f90432dc5f069b66bc5
fit2bot <68588906+fit2bot@users.noreply.github.com>
2020-07-24 10:20:37 +0800

    fix(perms): 优化授权列表添加刷新按钮 (#245)
    * fix(perms): 优化授权列表添加刷新按钮
    
    * fix(perms): 优化授权列表添加导入导出按钮
    
    Co-authored-by: jym503558564 <503558564@qq.com>

--- src/views/perms/DatabaseAppPermission/DatabaseAppPermissionList.vue
+++ src/views/perms/DatabaseAppPermission/DatabaseAppPermissionList.vue
@@ -63,7 +63,6 @@ export default {
         }
       },
       headerActions: {
-        hasRightActions: false,
         hasBulkDelete: false
       }
     }
--- src/views/perms/RemoteAppPermission/RemoteAppPermissionList.vue
+++ src/views/perms/RemoteAppPermission/RemoteAppPermissionList.vue
@@ -67,7 +67,6 @@ export default {
         }
       },
       headerActions: {
-        hasRightActions: false,
         hasBulkDelete: false
       }
     }
