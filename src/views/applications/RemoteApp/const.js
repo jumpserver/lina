1c35f994de649480ef552fec519d79507d94e2ae
OrangeM21 <orangemtony@gmail.com>
2020-07-28 01:54:49 +0800

    fix: 修复创建远程应用密码框显示明文的问题
    Closes https://github.com/jumpserver/trello/issues/133

--- src/views/applications/RemoteApp/const.js
+++ src/views/applications/RemoteApp/const.js
@@ -18,7 +18,7 @@ export const REMOTE_APP_TYPE_FIELDS_MAP = {
       label: i18n.t('applications.chrome_username')
     },
     {
-      id: 'chrome_password', el: {}, attrs: {}, type: 'input', prop: 'chrome_password',
+      id: 'chrome_password', el: { 'show-password': true }, attrs: {}, type: 'input', prop: 'chrome_password',
       label: i18n.t('applications.chrome_password')
     }
   ],
@@ -36,7 +36,7 @@ export const REMOTE_APP_TYPE_FIELDS_MAP = {
       label: i18n.t('applications.mysql_workbench_username')
     },
     {
-      id: 'mysql_workbench_password', el: {}, attrs: {}, type: 'input', prop: 'mysql_workbench_password',
+      id: 'mysql_workbench_password', el: { 'show-password': true }, attrs: {}, type: 'input', prop: 'mysql_workbench_password',
       label: i18n.t('applications.mysql_workbench_password')
     }
   ],
@@ -50,7 +50,7 @@ export const REMOTE_APP_TYPE_FIELDS_MAP = {
       label: i18n.t('applications.vmware_username')
     },
     {
-      id: 'vmware_password', el: {}, attrs: {}, type: 'input', prop: 'vmware_password',
+      id: 'vmware_password', el: { 'show-password': true }, attrs: {}, type: 'input', prop: 'vmware_password',
       label: i18n.t('applications.vmware_password')
     }
   ],
@@ -68,7 +68,7 @@ export const REMOTE_APP_TYPE_FIELDS_MAP = {
       label: i18n.t('applications.custom_username')
     },
     {
-      id: 'custom_password', el: {}, attrs: {}, type: 'input', prop: 'custom_password',
+      id: 'custom_password', el: { 'show-password': true }, attrs: {}, type: 'input', prop: 'custom_password',
       label: i18n.t('applications.custom_password')
     }
   ]
