50f5e05bd01635869700333748737bca8f323f98
OrangeM21 <orangemtony@gmail.com>
2020-08-08 01:11:36 +0800

    feat(kubenetes): 添加kubenetes应用

---
+++ src/views/applications/KubernetesApp/KubernetesAppCreateUpdate.vue
@@ -0,0 +1,37 @@
+<template>
+  <GenericCreateUpdatePage v-bind="$data" />
+</template>
+
+<script>
+import { GenericCreateUpdatePage } from '@/layout/components'
+export default {
+  components: {
+    GenericCreateUpdatePage
+  },
+  data() {
+    return {
+      initial: {
+        type: 'k8s'
+      },
+      fields: [
+        [this.$t('common.Basic'), ['name', 'type']],
+        [this.$t('applications.kubernetes'), ['cluster']],
+        [this.$t('common.Other'), ['comment']]
+      ],
+      fieldsMeta: {
+        type: {
+          disabled: true
+        }
+      },
+      url: '/api/v1/applications/k8s-apps/'
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
