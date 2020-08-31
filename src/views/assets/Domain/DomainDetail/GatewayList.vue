868e77c983efe8ffd79931aa8dbd41ff15e792eb
Orange <orangemtony@gmail.com>
2020-08-31 18:53:39 +0800

    feat: 添加测试网关弹窗
    Closes https://github.com/jumpserver/trello/issues/191

--- src/views/assets/Domain/DomainDetail/GatewayList.vue
+++ src/views/assets/Domain/DomainDetail/GatewayList.vue
@@ -1,13 +1,41 @@
 <template>
-  <ListTable :table-config="tableConfig" :header-actions="headerActions" />
+  <div>
+    <ListTable :table-config="tableConfig" :header-actions="headerActions" />
+    <Dialog
+      v-if="dialogVisible"
+      :title="this.$t('assets.TestGatewayTestConnection')"
+      :visible.sync="dialogVisible"
+      width="40%"
+      top="35vh"
+      :show-confirm="false"
+      :show-cancel="false"
+      :destroy-on-close="true"
+    >
+      <el-row :gutter="20">
+        <el-col :span="4">
+          <div style="line-height: 34px;text-align: center">{{ $t('assets.SshPort') }}</div>
+        </el-col>
+        <el-col :span="14">
+          <el-input v-model="portInput" />
+          <span class="help-tips help-block">{{ $t('assets.TestGatewayHelpMessage') }}</span>
+        </el-col>
+        <el-col :span="4">
+          <el-button size="mini" type="primary" style="line-height:20px " :loading="buttonLoading" @click="dialogConfirm">{{ this.$t('common.Confirm') }}</el-button>
+        </el-col>
+      </el-row>
+    </Dialog>
+  </div>
+
 </template>
 
 <script>
 import ListTable from '@/components/ListTable/index'
 import DisplayFormatter from '@/components/ListTable/formatters/DisplayFormatter'
+import Dialog from '@/components/Dialog'
 export default {
   components: {
-    ListTable
+    ListTable,
+    Dialog
   },
   props: {
     object: {
@@ -48,15 +76,14 @@ export default {
                   name: 'TestConnection',
                   title: this.$t('assets.TestConnection'),
                   callback: function(val) {
+                    this.dialogVisible = true
                     if (!val.row.port) {
                       return this.$message.error(this.$t('common.BadRequestErrorMsg'))
+                    } else {
+                      this.portInput = val.row.port
+                      this.cellValue = val.cellValue
                     }
-                    this.$axios.post(`/api/v1/assets/gateways/${val.cellValue}/test-connective/`, { port: val.row.port }).then(
-                      res => {
-                        return this.$message.success(this.$t('common.TestSuccessMsg'))
-                      }
-                    )
-                  }
+                  }.bind(this)
                 }
               ]
             }
@@ -73,7 +100,27 @@ export default {
             domain: this.object.id
           }
         }
+      },
+      dialogVisible: false,
+      portInput: '',
+      cellValue: '',
+      buttonLoading: false
+    }
+  },
+  methods: {
+    dialogConfirm() {
+      this.buttonLoading = true
+      this.$axios.post(`/api/v1/assets/gateways/${this.cellValue}/test-connective/`, { port: this.portInput }).then(
+        res => {
+          return this.$message.success(this.$t('common.TestSuccessMsg'))
+        }
+      ).finally(() => {
+        this.portInput = ''
+        this.cellValue = ''
+        this.buttonLoading = false
+        this.dialogVisible = false
       }
+      )
     }
   }
 }
