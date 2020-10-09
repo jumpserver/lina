1b49c8993dcc388d4bd7a10b0ab63684214df014
Orange <orangemtony@gmail.com>
2020-10-09 19:29:03 +0800

    fix: 调整badge位置

--- src/layout/components/NavHeader/Tickets.vue
+++ src/layout/components/NavHeader/Tickets.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <el-link class="el-link" target="_blank" @click="goToTickets">{{ $t('route.Ticket') }}</el-link>
-    <el-badge v-if="getBadgeValue() !== 0" :value="getBadgeValue()" size="mini" type="primary" />
+    <!--    <el-link class="el-link" target="_blank" @click="goToTickets">{{ $t('route.Ticket') }}</el-link>-->
+    <el-badge v-if="getBadgeValue() !== 0" :value="getBadgeValue()" size="mini" type="primary">
+      <el-link class="el-link" target="_blank" @click="goToTickets">{{ $t('route.Ticket') }}</el-link>
+    </el-badge>
   </div>
 </template>
 
@@ -39,5 +41,8 @@ export default {
   color: #606266 !important;
   font-size: 13px;
   font-weight: 400
+}
+.el-badge ::v-deep .el-badge__content.is-fixed{
+  top:10px;
 }
 </style>
