37153ebe1dc2a610345092cfb30c00f8099bb963
Orange <orangemtony@gmail.com>
2021-01-13 03:11:59 +0800

    fix: 修复用户界面加载问题

--- src/userviews/assets/index.vue
+++ src/userviews/assets/index.vue
@@ -162,10 +162,10 @@ export default {
   },
   methods: {
     refreshAllFavorites() {
-      this.tableConfig.columns[4].formatterArgs.loading = true
+      this.tableConfig.columns[this.tableConfig.columns.length - 1].formatterArgs.loading = true
       this.$axios.get('/api/v1/assets/favorite-assets/').then(resp => {
         this.allFavorites = resp
-        this.tableConfig.columns[4].formatterArgs.loading = false
+        this.tableConfig.columns[this.tableConfig.columns.length - 1].formatterArgs.loading = false
       })
     },
     addOrDeleteFavorite(assetId) {
