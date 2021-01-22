c40bd0a9abf7fb7f221740d6ae1fd392c65c2b2b
ibuler <ibuler@qq.com>
2021-01-22 19:52:02 +0800

    perf: 完成终端和安全setting

--- src/views/settings/Security.vue
+++ src/views/settings/Security.vue
@@ -3,13 +3,8 @@
     <GenericCreateUpdateForm
       :fields="selectFields"
       :url="url"
-      :initial="object"
-      :update-success-next-route="successUrl"
-      :clean-form-value="cleanFormValue"
-      :object="object"
       :fields-meta="fieldsMeta"
       :get-method="getMethod"
-      :on-perform-success="onPerformSuccess"
       :has-detail-in-msg="false"
     />
   </IBox>
@@ -17,141 +12,51 @@
 <script>
 import GenericCreateUpdateForm from '@/layout/components/GenericCreateUpdateForm'
 import { IBox } from '@/components'
-import { Required } from '@/components/DataForm/rules'
 export default {
   name: 'Security',
   components: {
     GenericCreateUpdateForm,
     IBox
   },
-  props: {
-    object: {
-      type: Object,
-      default: null
-    }
-  },
   data() {
-    var validatorLoginLimitCount = (rule, value, callback) => {
-      if (parseInt(value) < 3) {
-        return callback(new Error(this.$t('setting.validatorMessage.EnsureThisValueIsGreaterThanOrEqualTo3')))
-      }
-      callback()
-    }
-    var validatorLoginLimitTime = (rule, value, callback) => {
-      if (parseInt(value) < 5) {
-        return callback(new Error(this.$t('setting.validatorMessage.EnsureThisValueIsGreaterThanOrEqualTo5')))
-      }
-      callback()
-    }
-    var validatorPasswordMinLength = (rule, value, callback) => {
-      if (parseInt(value) < 6) {
-        return callback(new Error(this.$t('setting.validatorMessage.EnsureThisValueIsGreaterThanOrEqualTo6')))
-      }
-      callback()
-    }
     return {
       selectFields: [
-        [this.$t('setting.SecuritySetting'), ['SECURITY_MFA_AUTH', 'SECURITY_COMMAND_EXECUTION', 'SECURITY_SERVICE_ACCOUNT_REGISTRATION',
-          'SECURITY_LOGIN_LIMIT_COUNT', 'SECURITY_LOGIN_LIMIT_TIME', 'SECURITY_MAX_IDLE_TIME',
-          'SECURITY_PASSWORD_EXPIRATION_TIME']],
-        [this.$t('setting.PasswordCheckRule'), ['SECURITY_PASSWORD_MIN_LENGTH', 'SECURITY_PASSWORD_UPPER_CASE',
-          'SECURITY_PASSWORD_LOWER_CASE', 'SECURITY_PASSWORD_NUMBER', 'SECURITY_PASSWORD_SPECIAL_CHAR']],
-        [this.$t('setting.Insecure_Command_Alert'), ['SECURITY_INSECURE_COMMAND', 'SECURITY_INSECURE_COMMAND_EMAIL_RECEIVER']]
+        [
+          this.$t('common.Basic'),
+          [
+            'SECURITY_COMMAND_EXECUTION', 'SECURITY_SERVICE_ACCOUNT_REGISTRATION',
+            'SECURITY_MAX_IDLE_TIME'
+          ]
+        ],
+        [
+          this.$t('common.Auth'),
+          [
+            'SECURITY_MFA_AUTH', 'SECURITY_LOGIN_LIMIT_COUNT', 'SECURITY_LOGIN_LIMIT_TIME',
+            'SECURITY_PASSWORD_EXPIRATION_TIME'
+          ]
+        ],
+        [
+          this.$t('setting.PasswordCheckRule'),
+          [
+            'SECURITY_PASSWORD_MIN_LENGTH', 'SECURITY_PASSWORD_UPPER_CASE',
+            'SECURITY_PASSWORD_LOWER_CASE', 'SECURITY_PASSWORD_NUMBER', 'SECURITY_PASSWORD_SPECIAL_CHAR'
+          ]
+        ],
+        [
+          this.$t('setting.Insecure_Command_Alert'),
+          [
+            'SECURITY_INSECURE_COMMAND', 'SECURITY_INSECURE_COMMAND_EMAIL_RECEIVER'
+          ]
+        ]
       ],
-      successUrl: { name: 'Settings', params: { activeMenu: 'Security' }},
       fieldsMeta: {
-        SECURITY_MFA_AUTH: {
-          label: this.$t('setting.securityMfaAuth'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.securityMfaAuth')
-        },
-        SECURITY_COMMAND_EXECUTION: {
-          label: this.$t('setting.securityCommandExecution'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.securityCommandExecution')
-        },
-        SECURITY_SERVICE_ACCOUNT_REGISTRATION: {
-          label: this.$t('setting.securityServiceAccountRegistration'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.securityServiceAccountRegistration')
-        },
-        SECURITY_LOGIN_LIMIT_COUNT: {
-          label: this.$t('setting.securityLoginLimitCount'),
-          rules: [
-            Required,
-            { validator: validatorLoginLimitCount }
-          ]
-        },
-        SECURITY_LOGIN_LIMIT_TIME: {
-          label: this.$t('setting.securityLoginLimitTime'),
-          rules: [
-            Required,
-            { validator: validatorLoginLimitTime }
-          ],
-          helpText: this.$t('setting.helpText.securityLoginLimitTime')
-        },
-        SECURITY_MAX_IDLE_TIME: {
-          label: this.$t('setting.securityMaxIdleTime'),
-          helpText: this.$t('setting.helpText.securityMaxIdleTime')
-        },
-        SECURITY_PASSWORD_EXPIRATION_TIME: {
-          label: this.$t('setting.securityPasswordExpirationTime'),
-          rules: [Required],
-          helpText: this.$t('setting.helpText.securityPasswordExpirationTime')
-        },
-        SECURITY_PASSWORD_MIN_LENGTH: {
-          label: this.$t('setting.securityPasswordMinLength'),
-          rules: [
-            Required,
-            { validator: validatorPasswordMinLength }
-          ]
-        },
-        SECURITY_PASSWORD_UPPER_CASE: {
-          label: this.$t('setting.securityPasswordUpperCase'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.securityPasswordUpperCase')
-        },
-        SECURITY_PASSWORD_LOWER_CASE: {
-          label: this.$t('setting.securityPasswordLowerCase'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.securityPasswordLowerCase')
-        },
-        SECURITY_PASSWORD_NUMBER: {
-          label: this.$t('setting.securityPasswordNumber'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.securityPasswordNumber')
-        },
-        SECURITY_PASSWORD_SPECIAL_CHAR: {
-          label: this.$t('setting.securityPasswordSpecialChar'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.securityPasswordSpecialChar')
-        },
-        SECURITY_INSECURE_COMMAND: {
-          label: this.$t('setting.SecurityInsecureCommand'),
-          type: 'checkbox',
-          helpTips: this.$t('setting.helpTip.SecurityInsecureCommand')
-        },
-        SECURITY_INSECURE_COMMAND_EMAIL_RECEIVER: {
-          label: this.$t('setting.SecurityInsecureCommandEmailReceiver'),
-          helpText: this.$t('setting.helpText.SecurityInsecureCommandEmailReceiver')
-        }
       },
-      url: '/api/v1/settings/setting/'
+      url: '/api/v1/settings/setting/?category=security'
     }
   },
   methods: {
-    cleanFormValue(data) {
-      return {
-        security: data
-      }
-    },
     getMethod() {
       return 'put'
-    },
-    onPerformSuccess(res, method, vm) {
-      this.$emit('submitSuccess', res)
-      this.$message.success(this.$t('common.updateSuccessMsg'))
-      setTimeout(() => window.location.reload(), 500)
     }
   }
 }
--- src/views/settings/Terminal.vue
+++ src/views/settings/Terminal.vue
@@ -3,10 +3,6 @@
     <GenericCreateUpdateForm
       :fields="selectFields"
       :url="url"
-      :initial="object"
-      :update-success-next-route="successUrl"
-      :clean-form-value="cleanFormValue"
-      :object="object"
       :fields-meta="fieldsMeta"
       :get-method="getMethod"
       :has-detail-in-msg="false"
@@ -16,87 +12,38 @@
 <script>
 import GenericCreateUpdateForm from '@/layout/components/GenericCreateUpdateForm'
 import { IBox } from '@/components'
-import { Required } from '@/components/DataForm/rules'
 export default {
   name: 'Terminal',
   components: {
     GenericCreateUpdateForm,
     IBox
   },
-  props: {
-    object: {
-      type: Object,
-      default: null
-    }
-  },
   data() {
     return {
       selectFields: [
-        [this.$t('setting.basicSetting'), ['TERMINAL_PASSWORD_AUTH', 'TERMINAL_PUBLIC_KEY_AUTH',
-          'TERMINAL_ASSET_LIST_SORT_BY', 'TERMINAL_ASSET_LIST_PAGE_SIZE', 'TERMINAL_SESSION_KEEP_DURATION',
-          'TERMINAL_TELNET_REGEX']]
+        [
+          'KoKo', [
+            'TERMINAL_PASSWORD_AUTH', 'TERMINAL_PUBLIC_KEY_AUTH',
+            'TERMINAL_ASSET_LIST_SORT_BY', 'TERMINAL_ASSET_LIST_PAGE_SIZE',
+            'TERMINAL_TELNET_REGEX'
+          ]
+        ],
+        [
+          this.$t('common.Other'),
+          [
+            'TERMINAL_SESSION_KEEP_DURATION'
+          ]
+        ]
       ],
-      successUrl: { name: 'Settings', params: { activeMenu: 'Terminal' }},
       fieldsMeta: {
-        TERMINAL_PASSWORD_AUTH: {
-          label: this.$t('setting.terminalPasswordAuth'),
-          type: 'checkbox'
-        },
-        TERMINAL_PUBLIC_KEY_AUTH: {
-          label: this.$t('setting.terminalPublicKeyAuth'),
-          type: 'checkbox'
-        },
-        TERMINAL_HEARTBEAT_INTERVAL: {
-          label: this.$t('setting.terminalHeartbeatInterval'),
-          rules: [Required],
-          helpText: this.$t('setting.helpText.terminalHeartbeatInterval')
-        },
-        TERMINAL_ASSET_LIST_SORT_BY: {
-          label: this.$t('setting.terminalAssetListSortBy'),
-          type: 'select',
-          options: [
-            { label: this.$t('setting.Hostname'),
-              value: 'hostname'
-            },
-            {
-              label: 'IP',
-              value: 'ip'
-            }
-          ],
-          rules: [Required]
-        },
-        TERMINAL_ASSET_LIST_PAGE_SIZE: {
-          label: this.$t('setting.terminalAssetListPageSize'),
-          type: 'select',
-          options: [
-            { label: this.$t('setting.all'), value: 'all' },
-            { label: this.$t('setting.auto'), value: 'auto' },
-            { label: '10', value: '10' },
-            { label: '15', value: '15' },
-            { label: '25', value: '25' },
-            { label: '50', value: '50' }
-          ],
-          rules: [Required]
-        },
-        TERMINAL_SESSION_KEEP_DURATION: {
-          label: this.$t('setting.terminalSessionKeepDuration'),
-          rules: [Required],
-          helpText: this.$t('setting.helpText.terminalSessionKeepDuration')
-        },
         TERMINAL_TELNET_REGEX: {
-          label: this.$t('setting.terminalTelnetRegex'),
-          helpText: this.$t('setting.helpText.terminalTelnetRegex')
+          type: 'input'
         }
       },
-      url: '/api/v1/settings/setting/'
+      url: '/api/v1/settings/setting/?category=terminal'
     }
   },
   methods: {
-    cleanFormValue(data) {
-      return {
-        terminal: data
-      }
-    },
     getMethod() {
       return 'put'
     }
