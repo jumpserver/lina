ee1789b651eaa1898bda1c9e0fb71f9066517eb1
ibuler <ibuler@qq.com>
2021-02-26 15:52:40 +0800

    perf: 优化全局组织

--- src/layout/components/GenericListPage/index.vue
+++ src/layout/components/GenericListPage/index.vue
@@ -1,34 +1,16 @@
 <template>
   <Page v-bind="$attrs">
-    <ListTable ref="ListTable" v-bind="iAttrs" />
+    <GenericListTable ref="ListTable" v-bind="$attrs" />
   </Page>
 </template>
 
 <script>
 import Page from '@/layout/components/Page'
-import ListTable from '@/components/ListTable'
-import { mapGetters } from 'vuex'
+import GenericListTable from '@/layout/components/GenericListTable'
 export default {
   name: 'GenericListPage',
   components: {
-    Page, ListTable
-  },
-  data() {
-    return {}
-  },
-  computed: {
-    ...mapGetters(['currentOrg']),
-    iAttrs() {
-      const attrs = _.cloneDeep(this.$attrs)
-      const canCreate = _.get(attrs, 'header-actions.canCreate', null)
-      this.$log.debug('Can create: ', canCreate)
-      if (canCreate === null && this.currentOrg.is_root) {
-        _.set(attrs, 'header-actions.canCreate', false)
-      }
-      // attrs.headerActions.can
-      // console.log()
-      return attrs
-    }
+    Page, GenericListTable
   }
 }
 </script>
--- src/views/xpack/Cloud/Account/AccountList.vue
+++ src/views/xpack/Cloud/Account/AccountList.vue
@@ -1,14 +1,14 @@
 <template>
-  <ListTable :table-config="tableConfig" :header-actions="headerActions" />
+  <GenericListTable :table-config="tableConfig" :header-actions="headerActions" />
 </template>
 
 <script type="text/jsx">
-import ListTable from '@/components/ListTable'
+import GenericListTable from '@/layout/components/GenericListTable'
 import { BooleanFormatter, DetailFormatter } from '@/components/ListTable/formatters'
 
 export default {
   components: {
-    ListTable
+    GenericListTable
   },
   data() {
     const vm = this
