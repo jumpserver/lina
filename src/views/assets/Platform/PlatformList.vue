7548bcb66ef5f24ca6beae2f3ac543234b53e790
Orange <orangemtony@gmail.com>
2021-03-04 17:00:13 +0800

    fix: 修复平台列表默认可删除的错误

--- src/views/assets/Platform/PlatformList.vue
+++ src/views/assets/Platform/PlatformList.vue
@@ -4,7 +4,7 @@
 
 <script>
 import { GenericListPage } from '@/layout/components'
-
+import { ActionsFormatter } from '@/components/ListTable/formatters'
 export default {
   components: {
     GenericListPage
@@ -19,6 +19,26 @@ export default {
         columnsMeta: {
           base: {
             width: '140px'
+          },
+          actions: {
+            prop: 'actions',
+            align: 'center',
+            label: this.$t('assets.Action'),
+            formatter: ActionsFormatter,
+            width: '200px',
+            formatterArgs: {
+              canDelete: (row, vaule) => {
+                return !row.internal
+              },
+              canUpdate: (row, vaule) => {
+                return !row.internal
+              },
+              performDelete: ({ row, col }) => {
+                const id = row.id
+                const url = `/api/v1/assets/platforms/${id}/`
+                return this.$axios.delete(url)
+              }
+            }
           }
         }
       },
