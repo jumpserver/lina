7dc5ec8fa7cea5fa8182a64eeda37fb712be1af5
Orange <orangemtony@gmail.com>
2021-03-15 17:19:33 +0800

    fix: 删除管理用户详情页的冗余快捷功能

--- src/views/assets/AdminUser/AdminUserDetail/Detail.vue
+++ src/views/assets/AdminUser/AdminUserDetail/Detail.vue
@@ -3,22 +3,17 @@
     <el-col :span="14">
       <DetailCard :items="detailCardItems" />
     </el-col>
-    <el-col :span="10">
-      <RelationCard ref="RelationCard" type="info" v-bind="nodeRelationConfig" />
-    </el-col>
   </el-row>
 </template>
 
 <script>
 import DetailCard from '@/components/DetailCard'
-import RelationCard from '@/components/RelationCard'
 import { toSafeLocalDateStr } from '@/utils/common'
 
 export default {
   name: 'Detail',
   components: {
-    DetailCard,
-    RelationCard
+    DetailCard
   },
   props: {
     object: {
@@ -28,31 +23,7 @@ export default {
   },
   data() {
     return {
-      nodeRelationConfig: {
-        icon: 'fa-info',
-        title: this.$t('assets.ReplaceNodeAssetsAdminUserWithThis'),
-        objectsAjax: {
-          url: '/api/v1/assets/nodes/',
-          transformOption: (item) => {
-            return { label: item.full_value, value: item.id }
-          }
-        },
-        performAdd: (items) => {
-          const data = []
-          const relationUrl = `/api/v1/assets/admin-users/${this.object.id}/nodes/`
-          items.map(v => {
-            data.push(v.value)
-          })
-          return this.$axios.patch(relationUrl, { nodes: data }).then(res => {
-            this.$message.success(this.$t('common.updateSuccessMsg'))
-          }).catch(err => {
-            this.$message.error(this.$t('common.updateErrorMsg' + ' ' + err))
-          })
-        },
-        onAddSuccess: () => {
-          this.$refs.RelationCard.$refs.select2.clearSelected()
-        }
-      }
+
     }
   },
   computed: {
