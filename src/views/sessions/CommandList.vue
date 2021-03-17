a10a681f1902d3f718ac29db8c731d8fbebc12da
Orange <orangemtony@gmail.com>
2021-03-17 17:22:12 +0800

    perf: 不可用节点添加提示

--- src/views/sessions/CommandList.vue
+++ src/views/sessions/CommandList.vue
@@ -120,6 +120,15 @@ export default {
         showAssets: false,
         // ?assets=0不显示资产. =1显示资产
         treeUrl: `/api/v1/terminal/command-storages/tree/?real=1&date_from=${dateFrom}&date_to=${dateTo}`,
+        view: {
+          // 添加禁用颜色区分
+          fontCss: (treeId, treeNode) => {
+            if (treeNode.chkDisabled) {
+              return { opacity: '0.4' }
+            }
+            return {}
+          }
+        },
         callback: {
           onSelected: function(event, treeNode) {
             // 禁止点击根节点
@@ -127,7 +136,7 @@ export default {
               return
             }
             if (!treeNode.valid) {
-              this.$message.error('不可用')
+              this.$message.error(this.$t('sessions.EsDisabled'))
               return
             }
             this.tableConfig.url = `/api/v1/terminal/commands/?command_storage_id=${treeNode.id}`
