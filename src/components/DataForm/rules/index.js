30c1284a41edb60bcd853ebac36a3e7750b01e45
Orange <orangemtony@gmail.com>
2021-04-01 19:33:53 +0800

    fix: 完善页面邮箱地址校验规则
    Closes https://github.com/jumpserver/trello/issues/933

--- src/components/DataForm/rules/index.js
+++ src/components/DataForm/rules/index.js
@@ -8,7 +8,14 @@ export const RequiredChange = {
   required: true, message: i18n.t('common.fieldRequiredError'), trigger: 'change'
 }
 
+export const EmailCheck = {
+  type: 'email',
+  message: i18n.t('common.InputEmailAddress'),
+  trigger: ['blur', 'change']
+}
+
 export default {
   Required,
-  RequiredChange
+  RequiredChange,
+  EmailCheck
 }
