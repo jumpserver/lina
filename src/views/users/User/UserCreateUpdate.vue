02e204bc942733507e9aa1c52168b034c3c71b8d
Orange <orangemtony@gmail.com>
2021-04-01 15:15:02 +0800

    perf: 用户来源不是本地时禁用更新密码

--- src/views/users/User/UserCreateUpdate.vue
+++ src/views/users/User/UserCreateUpdate.vue
@@ -38,8 +38,8 @@ export default {
       url: '/api/v1/users/users/',
       fieldsMeta: {
         password_strategy: {
-          hidden: () => {
-            return this.$route.params.id
+          hidden: (formValue) => {
+            return this.$route.params.id || formValue.source !== 'local'
           }
         },
         update_password: {
@@ -58,7 +58,7 @@ export default {
             if (formValue.password_strategy) {
               return false
             }
-            return !formValue.update_password
+            return !formValue.update_password || formValue.source !== 'local'
           },
           el: {
             required: false
@@ -71,12 +71,12 @@ export default {
             if (formValue.set_public_key) {
               return true
             }
-            return this.$route.meta.action !== 'update'
+            return this.$route.meta.action !== 'update' || formValue.source !== 'local'
           }
         },
         public_key: {
           hidden: (formValue) => {
-            return !formValue.set_public_key
+            return !formValue.set_public_key || formValue.source !== 'local'
           }
         },
         role: {
