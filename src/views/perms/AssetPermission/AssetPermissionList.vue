05c06e4fc2d92c1af22a5275b49bd598e0b47ee7
吴小白 <296015668@qq.com>
2021-07-29 05:41:43 +0800

    feat: 资产授权添加批量删除

--- src/views/perms/AssetPermission/AssetPermissionList.vue
+++ src/views/perms/AssetPermission/AssetPermissionList.vue
@@ -105,6 +105,7 @@ export default {
         }
       },
       headerActions: {
+        hasBulkDelete: true,
         createRoute() {
           return {
             name: 'AssetPermissionCreate',
