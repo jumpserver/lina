18affa1bd18c1f8b238a35871bfaa740985b160b
吴小白 <296015668@qq.com>
2021-07-29 12:36:26 +0800

    perf: 添加排序

--- src/views/assets/Asset/AssetList.vue
+++ src/views/assets/Asset/AssetList.vue
@@ -120,7 +120,11 @@ export default {
             formatterArgs: {
               route: 'AssetDetail'
             },
-            showOverflowTooltip: true
+            showOverflowTooltip: true,
+            sortable: true
+          },
+          platform: {
+            sortable: true
           },
           protocols: {
             formatter: function(row) {
