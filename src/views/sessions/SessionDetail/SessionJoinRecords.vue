e4240d7a8f601817e28167ac72d6a193c84a8361
Jiangjie.Bai <32935519+BaiJiangJie@users.noreply.github.com>
2021-09-07 18:16:38 +0800

    feat: 会话详情添加活动页面 (#1003)
    * fix: 添加配置 SECURITY_SESSION_SHARE
    
    * fix: 添加会话活动页面
    
    * fix: 添加会话活动页面
    
    * fix: 添加会话活动页面
    
    * fix: 添加会话活动页面

---
+++ src/views/sessions/SessionDetail/SessionJoinRecords.vue
@@ -0,0 +1,50 @@
+<template>
+  <ListTable :table-config="tableConfig" :header-actions="headerActions" />
+</template>
+
+<script>
+import ListTable from '@/components/ListTable'
+
+export default {
+  name: 'SessionJoinRecords',
+  components: {
+    ListTable
+  },
+  props: {
+    object: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      tableConfig: {
+        url: `/api/v1/terminal/session-join-records/?session=${this.object.id}`,
+        columns: [
+          'joiner_display', 'verify_code', 'reason', 'is_success', 'is_finished',
+          'date_joined', 'date_left', 'org_name'
+        ],
+        columnsShow: {
+          min: ['joiner_display', 'is_success', 'reason'],
+          default: [
+            'joiner_display', 'verify_code', 'is_success', 'reason', 'is_finished', 'date_joined', 'date_left'
+          ]
+        },
+        columnsMeta: {
+        }
+      },
+      headerActions: {
+        hasImport: false,
+        hasLeftActions: false,
+        searchConfig: {
+          exclude: ['date_joined', 'date_left']
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
