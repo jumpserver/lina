0df93fe96a0340766f4804889306ecbc94587909
feng626 <1304903146@qq.com>
2021-09-10 10:49:50 +0800

    perf: sms样式修改

---
+++ src/views/settings/SMS/Base.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-button type="default" size="mini" @click="visible=true">{{ $t('setting.Config') }}</el-button>
+    <Dialog
+      :visible.sync="visible"
+      :title="title"
+      :destroy-on-close="true"
+      :show-cancel="false"
+      :show-confirm="false"
+      width="70%"
+      v-on="$listeners"
+      @confirm="onConfirm()"
+    >
+      <slot />
+    </Dialog>
+  </div>
+</template>
+
+<script>
+import Dialog from '@/components/Dialog'
+export default {
+  name: 'Base',
+  components: {
+    Dialog
+  },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    onConfirm() {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/settings/SMS/SMS.vue
@@ -0,0 +1,27 @@
+<template>
+  <BaseAuth :title="$t('setting.SMS')">
+    <SMSAlibaba v-if="smsType === 'alibaba'" />
+    <SMSTencent v-if="smsType === 'tencent'" />
+  </BaseAuth>
+</template>
+<script>
+
+import BaseAuth from './Base'
+import SMSAlibaba from './SMSAlibaba'
+import SMSTencent from './SMSTencent'
+
+export default {
+  name: 'SMS',
+  components: {
+    BaseAuth,
+    SMSAlibaba,
+    SMSTencent
+  },
+  props: {
+    smsType: {
+      type: String,
+      default: 'alibaba'
+    }
+  }
+}
+</script>
