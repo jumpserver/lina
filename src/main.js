7c1b341460c92b0bd14818222d95c0fcc2dfedba
“怀磊” <2280131253@qq.com>
2021-09-22 15:19:05 +0800

    perf: 增加webpack别名配置

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import 'elementCss'
+import locale from 'elementLocale'
 
 import '@/styles/index.scss' // global css
 
