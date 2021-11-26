d612eea97d08d97086cb4971d720bbddbe1cff0b
ibuler <ibuler@qq.com>
2021-11-26 15:16:38 +0800

    feat: 资产显示标签

---
+++ src/components/TableFormatters/TagsFormatter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <el-tag
+      v-for="tag of cellValue"
+      :key="tag"
+      :type="getTagType(tag)"
+      class="tag-formatter"
+      disable-transitions
+    >
+      <i class="fa fa-tag" /> {{ tag }}
+    </el-tag>
+  </div>
+</template>
+
+<script>
+import BaseFormatter from './base'
+export default {
+  name: 'TagsFormatter',
+  extends: BaseFormatter,
+  props: {
+    formatterArgsDefault: {
+      type: Object,
+      default() {
+        return {
+          getTagType(tag) {
+            return 'primary'
+          }
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      formatterArgs: Object.assign(this.formatterArgsDefault, this.col.formatterArgs)
+    }
+  },
+  methods: {
+    getTagType(tag) {
+      return this.formatterArgs.getTagType(tag)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.tag-formatter {
+  margin: 2px 0;
+}
+</style>
