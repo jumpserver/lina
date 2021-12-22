31e699bd2d2c34ba226f1fbc886f0f4850a65a63
Michael Bai <baijiangjie@gmail.com>
2021-12-22 17:39:08 +0800

    feat: 应用授权增加Action动作控制

--- src/views/perms/components/PermissionFormActionField.vue
+++ src/views/perms/components/PermissionFormActionField.vue
@@ -1,6 +1,6 @@
 <template>
   <el-tree
-    :data="data"
+    :data="iData"
     show-checkbox
     node-key="id"
     :default-expand-all="false"
@@ -13,11 +13,15 @@
 
 <script>
 export default {
-  name: 'AssetPermissionFormActionField',
+  name: 'PermissionFormActionField',
   props: {
     value: {
       type: Array,
-      default: () => ['all', 'connect', 'upload_file', 'download_file', 'updownload', 'clipboard_copy_paste', 'clipboard_copy', 'clipboard_paste']
+      default: () => []
+    },
+    choices: {
+      type: Array,
+      default: () => []
     }
   },
   data() {
@@ -26,7 +30,7 @@ export default {
         children: 'children',
         label: 'label'
       },
-      data: [
+      fullChoicesTreeNodes: [
         {
           id: 'all',
           label: this.$t('perms.all'),
@@ -68,7 +72,34 @@ export default {
       ]
     }
   },
+  computed: {
+    choicesIDs() {
+      return this.choices.map((v) => v.value)
+    },
+    iData() {
+      this.$log.debug('choices: ', this.choicesIDs)
+      const fullTreeNodes = _.cloneDeep(this.fullChoicesTreeNodes)
+      const treeNodes = this.trimChoicesTreeNodes(fullTreeNodes)
+      this.$log.debug('choicesTreeNodes: ', treeNodes)
+      return treeNodes
+    }
+  },
   methods: {
+    trimChoicesTreeNodes(treeNodes) {
+      const newTreeNodes = []
+      for (const treeNode of treeNodes) {
+        if (!this.choicesIDs.includes(treeNode.id)) {
+          continue
+        }
+        let children = treeNode.children || []
+        if (children.length !== 0) {
+          children = this.trimChoicesTreeNodes(children)
+          treeNode.children = children
+        }
+        newTreeNodes.push(treeNode)
+      }
+      return newTreeNodes
+    },
     handleCheckChange(data, obj) {
       const checkedKeys = obj.checkedKeys
       if (checkedKeys.length !== 0) {
