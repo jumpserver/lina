84669b7cf038a2677d0531e2c92e34e409fe2f8b
ibuler <ibuler@qq.com>
2022-01-18 19:52:20 +0800

    perf: 优化 role user 处理

--- src/views/users/Role/RoleDetail/RoleInfo.vue
+++ src/views/users/Role/RoleDetail/RoleInfo.vue
@@ -3,22 +3,17 @@
     <el-col :md="14" :sm="24">
       <DetailCard :items="detailItems" />
     </el-col>
-    <el-col :md="10" :sm="24">
-      <RelationCard v-bind="relationConfig" />
-    </el-col>
   </el-row>
 </template>
 
 <script>
 import DetailCard from '@/components/DetailCard'
-import RelationCard from '@/components/RelationCard'
 import { toSafeLocalDateStr } from '@/utils/common'
 
 export default {
   name: 'RoleInfo',
   components: {
-    DetailCard,
-    RelationCard
+    DetailCard
   },
   props: {
     object: {
@@ -28,34 +23,6 @@ export default {
   },
   data() {
     return {
-      relationConfig: {
-        icon: 'fa-user',
-        title: this.$t('common.Members'),
-        objectsAjax: {
-          url: '/api/v1/users/users/?fields_size=mini&order=name',
-          transformOption: (item) => {
-            return { label: item.name + '(' + item.username + ')', value: item.id }
-          }
-        },
-        hasObjectsId: this.object.users,
-        performDelete: (item) => {
-          const itemId = item.value
-          const objectId = this.object.id
-          const relationUrl = `/api/v1/users/users-groups-relations/?usergroup=${objectId}&user=${itemId}`
-          return this.$axios.delete(relationUrl)
-        },
-        performAdd: (items) => {
-          const relationUrl = `/api/v1/users/users-groups-relations/`
-          const objectId = this.object.id
-          const data = items.map(v => {
-            return {
-              user: v.value,
-              usergroup: objectId
-            }
-          })
-          return this.$axios.post(relationUrl, data)
-        }
-      }
     }
   },
   computed: {
@@ -78,12 +45,6 @@ export default {
           value: this.object.comment
         }
       ]
-    }
-  },
-  watch: {
-    group(iNew, iOld) {
-      this.$log.debug('Group has changed')
-      this.relationConfig.hasObjectsId = iNew.users
     }
   },
   methods: {
--- src/views/users/Role/RoleDetail/RoleUsers.vue
+++ src/views/users/Role/RoleDetail/RoleUsers.vue
@@ -1,15 +1,24 @@
 <template>
-  <ListTable
-    :table-config="tableConfig"
-    :header-actions="headerActions"
-  />
+  <el-row :gutter="20">
+    <el-col :md="14" :sm="24">
+      <ListTable
+        :table-config="tableConfig"
+        :header-actions="headerActions"
+      />
+    </el-col>
+    <el-col :md="10" :sm="24">
+      <RelationCard v-bind="relationConfig" />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-import { ListTable } from '@/components'
+import { ListTable, RelationCard } from '@/components'
+
 export default {
   components: {
-    ListTable
+    ListTable,
+    RelationCard
   },
   props: {
     object: {
@@ -20,22 +29,51 @@ export default {
   data() {
     return {
       loading: true,
+      relationConfig: {
+        icon: 'fa-user',
+        title: this.$t('common.Members'),
+        objectsAjax: {
+          url: '/api/v1/users/users/?fields_size=mini&order=name',
+          transformOption: (item) => {
+            return { label: item.name + '(' + item.username + ')', value: item.id }
+          }
+        },
+        performAdd: (items) => {
+          const relationUrl = `/api/v1/rbac/${this.object.scope}-role-bindings/`
+          const objectId = this.object.id
+          const data = items.map(v => {
+            return {
+              user: v.value,
+              role: objectId,
+              scope: this.object.scope
+            }
+          })
+          return this.$axios.post(relationUrl, data)
+        }
+      },
       tableConfig: {
         url: `/api/v1/rbac/${this.object.scope}-role-bindings/?role=${this.object.id}`,
-        columns: ['user_display', 'actions']
+        columns: ['user_display', 'actions'],
+        columnsMeta: {
+          actions: {
+            formatterArgs: {
+              hasUpdate: false,
+              hasClone: false
+            }
+          }
+        }
       },
       headerActions: {
         hasLeftActions: false
       }
     }
   },
-  methods: {
-  }
+  methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
-.perm-tree >>> .ztree {
+.perm-tree > > > .ztree {
 }
 
 </style>
