1c802e6213486f048efe3fd9a78087b75bde2c3d
feng626 <1304903146@qq.com>
2022-02-23 19:18:36 +0800

    fix: 云导入router perm

--- src/router/console/xpack.js
+++ src/router/console/xpack.js
@@ -8,7 +8,8 @@ export default [
     redirect: '',
     meta: {
       title: i18n.t('xpack.Cloud.CloudImport'),
-      permissions: ['xpack.view_account']
+      app: 'xpack',
+      resource: 'account'
     },
     children: [
       {
@@ -18,8 +19,7 @@ export default [
         hidden: true,
         meta: {
           title: i18n.t('xpack.Cloud.CloudImport'),
-          activeMenu: '/console/assets/assets',
-          permissions: ['xpack.view_account']
+          activeMenu: '/console/assets/assets'
         }
       },
       {
