e52d50dba01926b1e4464a7a41cc2fb31084a8f5
“怀磊” <2280131253@qq.com>
2022-03-02 10:08:12 +0800

    feat: 配置概况页的路由权限

--- src/router/audit/index.js
+++ src/router/audit/index.js
@@ -25,7 +25,9 @@ export default {
       meta: {
         icon: 'dashboard',
         title: i18n.t('route.Overview'),
-        permissions: []
+        permissions: ['common.view_resourcestatistics'],
+        resource: 'common',
+        app: 'common'
       }
     },
     {
