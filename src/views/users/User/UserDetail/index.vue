be324929c87aaed8e0c1da9635535b562c0e24bb
“怀磊” <2280131253@qq.com>
2022-03-02 16:46:56 +0800

    feat: 用户详情tab页rbac控制

--- src/views/users/User/UserDetail/index.vue
+++ src/views/users/User/UserDetail/index.vue
@@ -34,6 +34,7 @@ export default {
     UserLoginACLList
   },
   data() {
+    const vm = this
     return {
       user: { name: '', username: '', email: '', comment: '' },
       config: {
@@ -47,23 +48,28 @@ export default {
           },
           {
             title: this.$t('users.tabs.grantedAssets'),
-            name: 'UserGrantedAssets'
+            name: 'UserGrantedAssets',
+            hidden: () => !vm.$hasPerm('perms.view_myassets')
           },
           {
             title: this.$t('users.tabs.assetPermissionRules'),
-            name: 'UserAssetPermissionRules'
+            name: 'UserAssetPermissionRules',
+            hidden: () => !vm.$hasPerm('perms.view_assetpermission')
           },
           {
             title: this.$t('users.tabs.grantedApplications'),
-            name: 'UserGrantedApplications'
+            name: 'UserGrantedApplications',
+            hidden: () => !vm.$hasPerm('perms.view_usergroupapps')
           },
           {
             title: this.$t('users.tabs.ApplicationPermissionRules'),
-            name: 'UserApplicationPermissionRules'
+            name: 'UserApplicationPermissionRules',
+            hidden: () => !vm.$hasPerm('perms.view_applicationpermission')
           },
           {
             title: this.$t('route.UserAclLists'),
-            name: 'UserLoginACLList'
+            name: 'UserLoginACLList',
+            hidden: () => !vm.$hasPerm('acls.view_loginacl')
           }
         ]
       }
