3c75ab58e41e32d0796ca9e674f4bd7220b24fcf
fit2bot <68588906+fit2bot@users.noreply.github.com>
2022-03-10 18:25:39 +0800

    fix: 修复资产详情中的权限判断 (#1437)
    * fix: 修复资产详情中的权限判断
    
    * fix: change
    
    Co-authored-by: ibuler <ibuler@qq.com>

--- src/router/common.js
+++ src/router/common.js
@@ -10,13 +10,19 @@ export default [
     component: () => import('@/views/ops/CeleryTaskLog'),
     name: 'CeleryTaskLog',
     hidden: true,
-    meta: { title: i18n.t('route.CeleryTaskLog'), roles: ['SuperAdmin', 'Admin', 'Auditor', 'User'] }
+    meta: {
+      title: i18n.t('route.CeleryTaskLog'),
+      permissions: []
+    }
   },
   {
     path: '/ops/task/task/:id/log/',
     component: () => import('@/views/ops/CeleryTaskLog'),
     name: 'TaskLog',
     hidden: true,
-    meta: { title: i18n.t('route.CeleryTaskLog'), roles: ['SuperAdmin', 'Admin', 'Auditor', 'User'] }
+    meta: {
+      title: i18n.t('route.CeleryTaskLog'),
+      permissions: []
+    }
   }
 ]
