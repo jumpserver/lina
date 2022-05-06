4a8305979eff80eaa3c66c8cd3e5dddf943f2ed8
Jiangjie.Bai <bugatti_it@163.com>
2022-05-06 17:09:28 +0800

    feat: public settings 区分 public 和 open

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -32,24 +32,25 @@ const actions = {
     commit('CHANGE_SETTING', data)
   },
   // get user Profile
-  getPublicSettings({ commit, state }) {
+  getPublicSettings({ commit, state }, isOpen) {
     return new Promise((resolve, reject) => {
-      getPublicSettings().then(response => {
-        const data = response.data || {}
-        const faviconURL = data['LOGO_URLS']?.favicon
-        let link = document.querySelector("link[rel*='icon']")
-        if (!link) {
-          link = document.createElement('link')
-          link.type = 'image/x-icon'
-          link.rel = 'shortcut icon'
-          document.getElementsByTagName('head')[0].appendChild(link)
+      getPublicSettings(isOpen).then(response => {
+        const data = response || {}
+        if (isOpen) {
+          const faviconURL = data['LOGO_URLS']?.favicon
+          let link = document.querySelector("link[rel*='icon']")
+          if (!link) {
+            link = document.createElement('link')
+            link.type = 'image/x-icon'
+            link.rel = 'shortcut icon'
+            document.getElementsByTagName('head')[0].appendChild(link)
+          }
+          if (faviconURL) {
+            link.href = faviconURL
+          }
+          // 动态修改Title
+          document.title = data['LOGIN_TITLE']
         }
-        if (faviconURL) {
-          link.href = faviconURL
-        }
-
-        // 动态修改Title
-        document.title = data['LOGIN_TITLE']
         commit('SET_PUBLIC_SETTINGS', data)
         resolve(response)
       }).catch(error => {
