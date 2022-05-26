9107c6a76a06bad141cb167a18af28f775c3a990
“huailei000” <2280131253@qq.com>
2022-05-26 11:07:34 +0800

    feat: 命令过滤详情添加用户、用户组、应用右侧卡片快捷入口

--- src/views/assets/CommandFilter/CommandFilterDetail/Detail.vue
+++ src/views/assets/CommandFilter/CommandFilterDetail/Detail.vue
@@ -4,7 +4,10 @@
       <DetailCard :items="detailCardItems" />
     </el-col>
     <el-col :md="10" :sm="24">
-      <RelationCard ref="systemUserRelation" v-bind="systemUserRelationConfig" />
+      <RelationCard ref="users" v-bind="userRelationConfig" />
+      <RelationCard ref="userGroups" type="info" v-bind="userGroupsRelationConfig" class="card-margin" />
+      <RelationCard ref="applications" type="warning" v-bind="applicationsRelationConfig" class="card-margin" />
+      <RelationCard ref="systemUsers" type="danger" v-bind="systemUserRelationConfig" class="card-margin" />
     </el-col>
   </el-row>
 </template>
@@ -26,42 +29,58 @@ export default {
     }
   },
   data() {
+    const disabled = !this.$hasPerm('assets.change_commandfilter')
+    const defaultTransformOption = (item, filed) => {
+      const currentFiled = item[filed] ? item[filed] : filed === 'username' ? '*' : ''
+      return { label: item.name + '(' + currentFiled + ')', value: item.id }
+    }
     return {
+      userRelationConfig: {
+        disabled,
+        icon: 'fa-user',
+        title: this.$t('common.User'),
+        objectsAjax: {
+          url: '/api/v1/users/users/?fields_size=mini',
+          transformOption: (item) => defaultTransformOption(item, 'username')
+        },
+        hasObjectsId: this.object.users,
+        performAdd: item => this.performAddHandle(item, 'users'),
+        performDelete: item => this.performDeleteHandle(item, 'users')
+      },
+      userGroupsRelationConfig: {
+        disabled,
+        icon: 'fa-users',
+        title: this.$t('users.UserGroups'),
+        objectsAjax: {
+          url: '/api/v1/users/groups/'
+        },
+        hasObjectsId: this.object.user_groups,
+        performAdd: item => this.performAddHandle(item, 'user_groups'),
+        performDelete: item => this.performDeleteHandle(item, 'user_groups')
+      },
+      applicationsRelationConfig: {
+        disabled,
+        icon: 'fa-th',
+        title: this.$t('assets.Applications'),
+        objectsAjax: {
+          url: `/api/v1/applications/applications/?category__in=db,cloud`,
+          transformOption: (item) => defaultTransformOption(item, 'type_display')
+        },
+        hasObjectsId: this.object.applications,
+        performAdd: item => this.performAddHandle(item, 'applications'),
+        performDelete: item => this.performDeleteHandle(item, 'applications')
+      },
       systemUserRelationConfig: {
-        icon: 'fa-info',
-        title: this.$t('perms.addSystemUserToThisPermission'),
+        disabled,
+        icon: 'fa-info-circle',
+        title: this.$t('assets.SystemUser'),
         objectsAjax: {
           url: `/api/v1/assets/system-users/?protocol__in=ssh,telnet,mysql,postgresql,mariadb,oracle,sqlserver,k8s`,
-          transformOption: (item) => {
-            const username = item.username || '*'
-            return { label: item.name + '(' + username + ')', value: item.id }
-          }
+          transformOption: (item) => defaultTransformOption(item, 'username')
         },
         hasObjectsId: this.object.system_users,
-        performAdd: (items) => {
-          const newData = []
-          const value = this.$refs.systemUserRelation.iHasObjects
-          value.map(v => {
-            newData.push(v.value)
-          })
-          const relationUrl = `/api/v1/assets/cmd-filters/${this.object.id}/`
-          items.map(v => {
-            newData.push(v.value)
-          })
-          return this.$axios.patch(relationUrl, { system_users: newData })
-        },
-        performDelete: (item) => {
-          const itemId = item.value
-          const newData = []
-          const value = this.$refs.systemUserRelation.iHasObjects
-          value.map(v => {
-            if (v.value !== itemId) {
-              newData.push(v.value)
-            }
-          })
-          const relationUrl = `/api/v1/assets/cmd-filters/${this.object.id}/`
-          return this.$axios.patch(relationUrl, { system_users: newData })
-        }
+        performAdd: item => this.performAddHandle(item, 'system_users'),
+        performDelete: item => this.performDeleteHandle(item, 'system_users')
       }
     }
   },
@@ -90,10 +109,38 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    performAddHandle(item, updateField) {
+      const newDatas = []
+      const updateFieldRef = _.camelCase(updateField)
+      const options = this.$refs[updateFieldRef].iHasObjects
+      const relationUrl = `/api/v1/assets/cmd-filters/${this.object.id}/`
+      options.forEach(v => newDatas.push(v.value))
+      item.forEach(v => newDatas.push(v.value))
+
+      return this.$axios.patch(relationUrl, { [updateField]: newDatas })
+    },
+    performDeleteHandle(item, updateField) {
+      const newDatas = []
+      const itemId = item?.value || ''
+      const updateFieldRef = _.camelCase(updateField)
+      const options = this.$refs[updateFieldRef].iHasObjects
+      const relationUrl = `/api/v1/assets/cmd-filters/${this.object.id}/`
+      options.forEach(v => {
+        if (v.value !== itemId) {
+          newDatas.push(v.value)
+        }
+      })
+
+      return this.$axios.patch(relationUrl, { [updateField]: newDatas })
+    }
   }
 }
 </script>
 
 <style lang='less' scoped>
-
+  .card-margin {
+    margin-top: 15px;
+  }
 </style>
