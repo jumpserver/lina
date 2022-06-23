1bb0ccdb15ea6977ba0bf46ae034aff556a041ff
“huailei000” <2280131253@qq.com>
2022-06-23 15:33:04 +0800

    feat: 删除没用的css文件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'elementCss'
+import './styles/element-variables.scss'
 import locale from 'elementLocale'
 
 import '@/styles/index.scss' // global css
