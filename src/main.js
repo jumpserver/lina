0cf4c9cce397393d330d5cb86d72a956bd48fe36
ibuler <ibuler@qq.com>
2022-06-30 13:00:54 +0800

    perf: 修改主题

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
-import 'elementCss'
 import locale from 'elementLocale'
 
 import '@/styles/index.scss' // global css
