76d9a692200562e576625155060f47525b669ce9
ibuler <ibuler@qq.com>
2022-07-19 18:58:44 +0800

    perf: 修改 rdp

--- src/views/assets/SystemUser/SystemUserCreateUpdate/rdp.vue
+++ src/views/assets/SystemUser/SystemUserCreateUpdate/rdp.vue
@@ -28,20 +28,26 @@ export default {
       },
       fields: [
         [this.$t('common.Basic'), ['name', 'protocol', 'username', 'username_same_with_user']],
-        [this.$t('common.Auth'), ['login_mode', 'auto_generate_key', 'password', 'ad_domain']],
-        [this.$t('assets.AutoPush'), ['auto_push_account', 'system_groups']],
+        [this.$t('assets.Account'), [
+          'login_mode', 'account_template_enabled',
+          'auto_generate_key', 'password',
+          'auto_push_account', 'system_groups',
+          'ad_domain'
+        ]],
         [this.$t('common.Other'), ['priority', 'comment']]
       ],
       fieldsMeta: {
         login_mode: fields.login_mode,
         auto_generate_key: fields.auto_generate_key,
+        account_template_enabled: fields.account_template_enabled,
         username: fields.username,
         username_same_with_user: fields.username_same_with_user,
         auto_push_account: fields.auto_push_account,
         protocol: fields.protocol,
         ad_domain: {
           label: this.$t('assets.AdDomain'),
-          helpText: this.$t('assets.AdDomainHelpText')
+          helpText: this.$t('assets.AdDomainHelpText'),
+          hidden: (item) => item.protocol !== 'ssh' || !item.auto_push_account
         },
         password: fields.password,
         system_groups: fields.system_groups
