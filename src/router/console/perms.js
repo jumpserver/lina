85abefff74022b709dd15aa7e0434c06af4fd324
ibuler <ibuler@qq.com>
2022-08-24 16:17:36 +0800

    perf: 去掉 applications perm

--- src/router/console/perms.js
+++ src/router/console/perms.js
@@ -38,48 +38,5 @@ export default [
         meta: { title: i18n.t('route.AssetPermissionDetail') }
       }
     ]
-  },
-  {
-    path: 'application-permissions',
-    component: empty,
-    redirect: '',
-    meta: {
-      title: i18n.t('route.ApplicationPermission'),
-      resource: 'applicationpermission'
-    },
-    children: [
-      {
-        path: '',
-        name: 'ApplicationPermissionList',
-        component: () => import('@/views/perms/ApplicationPermission/ApplicationPermissionList'),
-        meta: {
-          title: i18n.t('route.ApplicationPermission')
-        }
-      },
-      {
-        path: 'create',
-        component: () => import('@/views/perms/ApplicationPermission/ApplicationPermissionCreateUpdate'),
-        name: 'ApplicationPermissionCreate',
-        hidden: true,
-        meta: {
-          title: i18n.t('route.ApplicationPermissionCreate'),
-          action: 'create'
-        }
-      },
-      {
-        path: ':id',
-        component: () => import('@/views/perms/ApplicationPermission/ApplicationPermissionDetail'),
-        name: 'ApplicationPermissionDetail',
-        hidden: true,
-        meta: { title: i18n.t('route.ApplicationPermissionDetail') }
-      },
-      {
-        path: ':id/update',
-        component: () => import('@/views/perms/ApplicationPermission/ApplicationPermissionCreateUpdate'),
-        name: 'ApplicationPermissionUpdate',
-        hidden: true,
-        meta: { title: i18n.t('route.ApplicationPermissionUpdate'), action: 'update' }
-      }
-    ]
   }
 ]
