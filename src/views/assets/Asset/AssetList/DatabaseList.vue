eb00b5f1cafba542c104168ce3fa05d439e61217
“huailei000” <2280131253@qq.com>
2022-08-29 18:18:25 +0800

    feat: 创建资产相关页面；配置路由；

---
+++ src/views/assets/Asset/AssetList/DatabaseList.vue
@@ -0,0 +1,42 @@
+<template>
+  <BaseList
+    :category="'database'"
+    :table-config="tableConfig"
+    :header-actions="headerActions"
+  />
+</template>
+
+<script>
+import BaseList from './components/BaseList'
+
+export default {
+  components: {
+    BaseList
+  },
+  data() {
+    return {
+      tableConfig: {
+        url: '/api/v1/assets/databases/',
+        columns: [
+          'name', 'ip', 'public_ip', 'admin_user_display',
+          'protocols', 'category', 'type', 'platform', 'sn',
+          'is_active', 'connectivity', 'labels_display',
+          'created_by', 'date_created', 'comment', 'org_name', 'actions'
+        ],
+        columnsMeta: {
+          platform: {
+            sortable: false
+          }
+        }
+      },
+      headerActions: {
+        createRoute: 'DatabaseCreate'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/assets/Asset/AssetList/NetworkList.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseList
+    :category="'networking'"
+    :table-config="tableConfig"
+    :header-actions="headerActions"
+  />
+</template>
+
+<script>
+import BaseList from './components/BaseList'
+
+export default {
+  components: {
+    BaseList
+  },
+  data() {
+    return {
+      tableConfig: {
+        url: '/api/v1/assets/networking/'
+      },
+      headerActions: {
+        createRoute: 'NetworkingCreate'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
