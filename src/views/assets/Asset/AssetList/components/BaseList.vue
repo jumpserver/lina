757c6ef44cb10e1e31bd6d114cd4501f9aaca116
“huailei000” <2280131253@qq.com>
2022-08-31 16:02:46 +0800

    fix: 修复切换tab路由参数不能添加问题；修复创建云平台页面空白问题；

--- src/views/assets/Asset/AssetList/components/BaseList.vue
+++ src/views/assets/Asset/AssetList/components/BaseList.vue
@@ -108,6 +108,9 @@ export default {
         onCreate: () => {
           this.showPlatform = true
         },
+        searchConfig: {
+          getUrlQuery: false
+        },
         extraMoreActions: [
           {
             name: 'DeactiveSelected',
