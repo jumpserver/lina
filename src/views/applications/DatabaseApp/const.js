f11fc947af90d6fb78a1891a446692bde9cff2cd
Jiangjie.Bai <bugatti_it@163.com>
2022-09-22 17:22:21 +0800

    feat: 修改 Endpoint 的创建页面; 修改 Oracle 数据库创建不包含版本号字段

--- src/views/applications/DatabaseApp/const.js
+++ src/views/applications/DatabaseApp/const.js
@@ -1,11 +1,9 @@
-import { ORACLE, MONGODB, REDIS } from '../const'
+import { MONGODB, REDIS } from '../const'
 
 export function getDatabaseTypeFieldsMap(type) {
   const baseParams = ['host', 'port', 'database']
   const tlsParams = ['use_ssl', 'ca_cert']
   switch (type) {
-    case ORACLE:
-      return baseParams.concat(['version'])
     case REDIS:
       return baseParams.concat(tlsParams.concat(['client_cert', 'cert_key']))
     case MONGODB:
--- src/views/settings/Terminal/Endpoint/EndpointList.vue
+++ src/views/settings/Terminal/Endpoint/EndpointList.vue
@@ -20,17 +20,15 @@ export default {
         url: '/api/v1/terminal/endpoints/',
         columns: [
           'name', 'host',
-          'http_port', 'https_port', 'ssh_port',
-          'rdp_port', 'mysql_port', 'mariadb_port',
-          'postgresql_port', 'redis_port',
-          'oracle_11g_port', 'oracle_12c_port',
+          'http_port', 'https_port', 'ssh_port', 'rdp_port',
+          'magnus_listen_port_range',
           'date_created', 'comment', 'actions'
         ],
         columnsShow: {
           min: ['name', 'actions'],
           default: [
             'name', 'host', 'actions',
-            'http_port', 'https_port', 'ssh_port', 'rdp_port'
+            'http_port', 'https_port', 'ssh_port', 'rdp_port', 'magnus_listen_port_range'
           ]
         },
         columnsMeta: {
