9067e0e42c1ab66aa86981be19f00d6ec57d2539
jiangweidong <weidong.jiang@fit2cloud.com>
2022-09-30 09:15:47 +0800

    feat: 操作日志重构-支持查看变更资源信息

---
+++ src/components/TableFormatters/TwoTabFormatter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="padding-left: 5%;">
+    <div v-if="isShowEmpty" style="text-align: center">
+      {{ this.$t('common.NoContent') }}
+    </div>
+    <div v-else>
+      <el-card v-if="row.before" class="box-card" style="width: 46%; display: inline-block">
+        <div slot="header">
+          <span>{{ this.$t('audits.BeforeChange') }}</span>
+        </div>
+        <div v-for="(value, key, index) in row.before" :key="index" class="text item">
+          {{ key }}: {{ value }}
+        </div>
+      </el-card>
+      <el-card v-if="row.after" class="box-card" style="width: 46%; display: inline-block">
+        <div slot="header">
+          <span>{{ this.$t('audits.AfterChange') }}</span>
+        </div>
+        <div v-for="(value, key, index) in row.after" :key="index" class="text item">
+          {{ key }}: {{ value }}
+        </div>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TwoTabFormatter',
+  props: {
+    row: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    isShowEmpty() {
+      const condition1 = !this.row.before && !this.row.after
+      const condition2 = JSON.stringify(this.row.after) === '{}' && JSON.stringify(this.row.before) === '{}'
+      return condition1 || condition2
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
