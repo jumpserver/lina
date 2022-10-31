97f708b9ef4b344d58bca0b78f3e8f18a0011bb1
“huailei000” <2280131253@qq.com>
2022-10-31 20:02:51 +0800

    fix: 修改快速更新参数

--- src/views/perms/AssetPermission/AssetPermissionDetail/AssetPermissionDetail.vue
+++ src/views/perms/AssetPermission/AssetPermissionDetail/AssetPermissionDetail.vue
@@ -32,7 +32,7 @@ export default {
       quickActions: [
         {
           title: this.$t('common.Active'),
-          type: 'switcher',
+          type: 'switch',
           attrs: {
             model: this.object.is_active,
             disabled: !this.$hasPerm('perms.change_assetpermission')
