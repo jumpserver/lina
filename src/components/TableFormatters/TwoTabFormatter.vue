19417f2a12cb0b4a67a5e8f7de744dbb5f6880a3
jiangweidong <weidong.jiang@fit2cloud.com>
2022-11-04 09:21:05 +0800

    feat: 修改显示样式

--- src/components/TableFormatters/TwoTabFormatter.vue
+++ src/components/TableFormatters/TwoTabFormatter.vue
@@ -1,25 +1,27 @@
 <template>
-  <div>
-    <div v-if="isShowEmpty" style="text-align: center">
+  <el-row :gutter="10">
+    <div v-if="isAllEmpty()" style="text-align: center">
       {{ this.$t('common.NoContent') }}
     </div>
     <div v-else>
-      <el-row :gutter="10">
-        <el-col :span="12">
+      <el-col :span="rightEmpty() ? 24 : 12">
+        <div v-if="!leftEmpty()">
           <el-alert type="info" effect="dark" :closable="false" style="width: 100%;">{{ row.leftTitle }}</el-alert>
           <div v-for="(value, key, index) in row.left" :key="index" class="text item">
-            <el-tag type="info" :hit="true" style="width: 100%">{{ key }}: {{ value }}</el-tag>
+            <el-tag type="info" :hit="true"><strong>{{ key }}: </strong>>{{ value }}</el-tag>
           </div>
-        </el-col>
-        <el-col :span="12">
+        </div>
+      </el-col>
+      <el-col :span="leftEmpty() ? 24 : 12">
+        <div v-if="!rightEmpty()">
           <el-alert type="success" effect="dark" :closable="false" style="width: 100%;">{{ row.rightTitle }}</el-alert>
           <div v-for="(value, key, index) in row.right" :key="index">
-            <el-tag type="info" :hit="true" style="width: 100%">{{ key }}: {{ value }}</el-tag>
+            <el-tag type="info" :hit="true"><strong>{{ key }}: </strong>{{ value }}</el-tag>
           </div>
-        </el-col>
-      </el-row>
+        </div>
+      </el-col>
     </div>
-  </div>
+  </el-row>
 </template>
 
 <script>
@@ -32,11 +34,18 @@ export default {
       default: () => ({})
     }
   },
-  computed: {
-    isShowEmpty() {
-      const condition1 = !this.row.left && !this.row.right
-      const condition2 = JSON.stringify(this.row.right) === '{}' && JSON.stringify(this.row.left) === '{}'
-      return condition1 || condition2
+  methods: {
+    isEmpty(content) {
+      return !content || JSON.stringify(content) === '{}'
+    },
+    leftEmpty() {
+      return this.isEmpty(this.row.left)
+    },
+    rightEmpty() {
+      return this.isEmpty(this.row.right)
+    },
+    isAllEmpty() {
+      return this.leftEmpty() && this.rightEmpty()
     }
   }
 }
@@ -44,5 +53,9 @@ export default {
 </script>
 
 <style scoped>
-
+  .el-tag{
+    width: 100%;
+    white-space: normal;
+    height:auto;
+}
 </style>
