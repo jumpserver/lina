f601a45700e9b42430c5e58f8a8cb0761390f42a
ibuler <ibuler@qq.com>
2022-11-08 20:58:01 +0800

    pref: 修改 perms account

--- src/views/perms/AssetPermission/components/AccountFormatter.vue
+++ src/views/perms/AssetPermission/components/AccountFormatter.vue
@@ -1,11 +1,10 @@
 <template>
-  <el-input v-model="input3" placeholder="请输入内容" class="input-with-select">
-    <el-select slot="prepend" v-model="select" class="select" placeholder="选择特殊账号">
-      <el-option label="所有账号" value="@ALL" />
-      <el-option label="手动输入" value="@INPUT" />
-      <el-option label="动态账号" value="@USER" />
-    </el-select>
-  </el-input>
+  <el-checkbox-group v-model="select">
+    <el-checkbox label="所有账号" value="@ALL" />
+    <el-checkbox label="手动输入" value="@INPUT" />
+    <el-checkbox label="同名账号" value="@USER" />
+    <el-checkbox label="指定输入" value="INPUT" />
+  </el-checkbox-group>
 </template>
 
 <script>
