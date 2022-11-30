179c420e9df72151bdd6826161caec260d5ba241
Bai <baijiangjie@gmail.com>
2022-11-30 22:09:12 +0800

    perf: 优化 Login Asset ACL 详情页面

--- src/views/acl/AssetAcl/AssetAclDetail/detail.vue
+++ src/views/acl/AssetAcl/AssetAclDetail/detail.vue
@@ -3,22 +3,17 @@
     <el-col :md="14" :sm="24">
       <DetailCard :items="detailCardItems" />
     </el-col>
-    <el-col :md="10" :sm="24">
-      <!--      <RelationCard ref="RelationCard" type="info" v-bind="nodeRelationConfig" />-->
-    </el-col>
   </el-row>
 </template>
 
 <script>
 import DetailCard from '@/components/DetailCard'
-// import RelationCard from '@/components/RelationCard'
 import { toSafeLocalDateStr } from '@/utils/common'
 
 export default {
   name: 'Detail',
   components: {
     DetailCard
-    // RelationCard
   },
   props: {
     object: {
@@ -29,31 +24,6 @@ export default {
   },
   data() {
     return {
-      nodeRelationConfig: {
-        icon: 'fa-info',
-        title: this.$t('assets.ReplaceNodeAssetsAdminUserWithThis'),
-        objectsAjax: {
-          url: '/api/v1/assets/nodes/',
-          transformOption: (item) => {
-            return { label: item['full_value'], value: item.id }
-          }
-        },
-        performAdd: (items) => {
-          const data = []
-          const relationUrl = `/api/v1/assets/admin-users/${this.object.id}/nodes/`
-          items.map(v => {
-            data.push(v.value)
-          })
-          return this.$axios.patch(relationUrl, { nodes: data }).then(res => {
-            this.$message.success(this.$tc('common.updateSuccessMsg'))
-          }).catch(err => {
-            this.$message.error(this.$tc('common.updateErrorMsg' + ' ' + err))
-          })
-        },
-        onAddSuccess: () => {
-          this.$refs.RelationCard.$refs.select2.clearSelected()
-        }
-      }
     }
   },
   computed: {
@@ -64,32 +34,28 @@ export default {
           value: this.object.name
         },
         {
-          key: this.$t('acl.username_group'),
+          key: this.$t('acl.user_username'),
           value: this.object.users.username_group.toString()
         },
         {
-          key: this.$t('acl.hostname_group'),
-          value: this.object.assets.hostname_group.toString()
+          key: this.$t('acl.asset_name'),
+          value: this.object.assets.name_group.toString()
         },
         {
-          key: this.$t('acl.asset_ip_group'),
-          value: this.object.assets.ip_group.toString()
+          key: this.$t('acl.asset_address'),
+          value: this.object.assets.address_group.toString()
         },
         {
-          key: this.$t('acl.system_users_name_group'),
-          value: this.object.system_users.name_group.toString()
-        },
-        {
-          key: this.$t('acl.system_users_protocol_group'),
-          value: this.object.system_users.protocol_group.toString()
-        },
-        {
-          key: this.$t('acl.system_users_username_group'),
-          value: this.object.system_users.username_group.toString()
+          key: this.$t('acl.account_username'),
+          value: this.object.accounts.username_group.toString()
         },
         {
           key: this.$t('acl.action'),
-          value: this.object.action_display
+          value: this.object.action.label
+        },
+        {
+          key: this.$t('acl.reviewer'),
+          value: this.object.reviewers.map((reviewer) => { return reviewer.name }).join(' | ')
         },
         {
           key: this.$t('acl.priority'),
