bef1e8f49a8522b376818445d8dd9c9de63e6b5c
Bai <baijiangjie@gmail.com>
2022-12-05 17:44:52 +0800

    perf: 修改 Domain 列表页面

--- src/views/assets/Domain/DomainList.vue
+++ src/views/assets/Domain/DomainList.vue
@@ -4,7 +4,6 @@
 
 <script>
 import { GenericListPage } from '@/layout/components'
-import { DetailFormatter } from '@/components/TableFormatters'
 
 export default {
   components: {
@@ -15,28 +14,26 @@ export default {
       tableConfig: {
         url: '/api/v1/assets/domains/',
         columns: [
-          'name', 'asset_count', 'application_count', 'gateway_count', 'date_created',
-          'comment', 'org_name', 'actions'
+          'name', 'asset_count', 'gateway_count', 'comment',
+          'date_created', 'org_name', 'actions'
         ],
         columnsShow: {
           min: ['name', 'actions'],
-          default: ['name', 'asset_count', 'application_count', 'gateway_count', 'comment', 'actions']
+          default: ['name', 'asset_count', 'gateway_count', 'comment', 'actions']
         },
         columnsMeta: {
           asset_count: {
-            label: this.$t('assets.Assets')
-          },
-          application_count: {
-            label: this.$t('assets.Applications')
+            prop: 'assets',
+            label: this.$t('assets.Assets'),
+            formatter: function(row) {
+              return <span> { row.assets.length } </span>
+            }
           },
           gateway_count: {
+            prop: 'gateways',
             label: this.$t('assets.Gateway'),
-            formatter: DetailFormatter,
-            formatterArgs: {
-              permissions: 'assets.view_gateway',
-              routeQuery: {
-                activeTab: 'GatewayList'
-              }
+            formatter: function(row) {
+              return <span> { row.gateways.length } </span>
             }
           }
         }
