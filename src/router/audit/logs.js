692141f239ec9c6f8caa278b7ef853672e8861fb
Aaron3S <chenyang@fit2cloud.com>
2022-12-16 15:51:34 +0800

    feat: 增加作业审计页面

--- src/router/audit/logs.js
+++ src/router/audit/logs.js
@@ -29,12 +29,12 @@ export default [
     }
   },
   {
-    path: 'command-execution-log',
-    name: 'tasksLog',
-    component: () => import('@/views/audits/CommandExecutionList'),
+    path: 'job-execution-log',
+    name: 'JobExecutionLog',
+    component: () => import('@/views/audits/JobExecutionLogList'),
     meta: {
-      title: i18n.t('route.tasksLog'),
-      permissions: ['ops.view_commandexecution']
+      title: i18n.t('route.JobExecutionLog'),
+      permissions: []
     }
   }
 ]
---
+++ src/views/audits/JobExecutionLogList.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <GenericListPage :table-config="tableConfig" :header-actions="headerActions" />
+  </div>
+</template>
+
+<script type="text/jsx">
+import GenericListPage from '@/layout/components/GenericListPage'
+import { getDaysAgo, getDaysFuture } from '@/utils/common'
+
+export default {
+  components: {
+    GenericListPage
+  },
+  data() {
+    const now = new Date()
+    const dateFrom = getDaysAgo(7, now).toISOString()
+    const dateTo = getDaysFuture(1, now).toISOString()
+    return {
+      tableConfig: {
+        url: '/api/v1/audits/job-logs/',
+        columns: [
+          'creator_name', 'is_finished',
+          'is_success', 'time_cost', 'date_start', 'date_finished'
+        ],
+        columnsMeta: {
+
+          time_cost: {
+            label: this.$t('ops.time'),
+            width: '100px',
+            formatter: function(row) {
+              if (row.time_cost) {
+                return row.time_cost.toFixed(2) + 's'
+              }
+              return '-'
+            }
+          },
+          is_finished: {
+            label: this.$t('ops.isFinished'),
+            width: '96px',
+            formatter: (row) => {
+              if (row.is_finished) {
+                return <i Class='fa fa-check text-primary'/>
+              }
+              return <i Class='fa fa-times text-danger'/>
+            },
+            formatterArgs: {
+              width: '14px'
+            }
+          },
+          is_success: {
+            label: this.$t('ops.isSuccess'),
+            width: '96px',
+            formatter: (row) => {
+              if (!row.is_finished) {
+                return <i Class='fa  fa fa-spinner fa-spin'/>
+              }
+              if (row.is_success) {
+                return <i Class='fa fa-check text-primary'/>
+              }
+              return <i Class='fa fa-times text-danger'/>
+            },
+            formatterArgs: {
+              width: '14px'
+            }
+          },
+          date_start: {
+            width: '160px'
+          }
+        },
+        extraQuery: {
+          date_to: dateTo,
+          date_from: dateFrom
+        }
+      },
+      headerActions: {
+        hasLeftActions: false,
+        hasDatePicker: true,
+        hasImport: false,
+        searchConfig: {
+          options: [
+            {
+              label: this.$t('audits.User'),
+              value: 'user__name'
+            },
+            {
+              label: this.$t('audits.Username'),
+              value: 'user__username'
+            },
+            {
+              label: this.$t('audits.SystemUserName'),
+              value: 'run_as__username'
+            }
+          ]
+        },
+        datePicker: {
+          dateStart: dateFrom,
+          dateEnd: dateTo
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
