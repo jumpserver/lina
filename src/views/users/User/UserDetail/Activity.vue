9624efa6fc3f4da9b72c297fc3c70a2a8c9d7664
jiangweidong <weidong.jiang@fit2cloud.com>
2023-01-10 14:07:57 +0800

    feat: 支持资源用户在详情页查看活动(Activities)时间线

---
+++ src/views/users/User/UserDetail/Activity.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :md="12" :sm="24">
+      <TimelineCard :title="userTitle" :url="userUrl" />
+    </el-col>
+    <el-col :md="12" :sm="24">
+      <TimelineCard :title="userLoginTitle" :url="userLoginUrl" />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import TimelineCard from '@/components/TimelineCard'
+
+export default {
+  name: 'UserActivity',
+  components: {
+    TimelineCard
+  },
+  props: {
+    object: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      userUrl: `/api/v1/audits/activities/?resource=operate_log&resource_id=${this.object.id}`,
+      userTitle: `${this.$t('common.Activity')} - ${this.$t('users.Users')} (${this.$t('common.Last8')})`,
+      userLoginUrl: `/api/v1/audits/activities/?resource=login&resource_id=${this.object.username}`,
+      userLoginTitle: `${this.$t('common.Activity')} - ${this.$t('users.Login')} (${this.$t('common.Last8')})`
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
