191f797615e5674e455b9898230a99078990d0f6
jiangweidong <weidong.jiang@fit2cloud.com>
2023-01-11 14:38:51 +0800

    feat: Redis/MongoDB 支持SSL

--- src/views/assets/Asset/AssetCreateUpdate/DatabaseCreateUpdate.vue
+++ src/views/assets/Asset/AssetCreateUpdate/DatabaseCreateUpdate.vue
@@ -4,6 +4,7 @@
 
 <script>
 import BaseAssetCreateUpdate from './BaseAssetCreateUpdate'
+import { UploadKey } from '@/components'
 
 export default {
   name: 'DatabaseCreateUpdate',
@@ -11,9 +12,55 @@ export default {
   data() {
     return {
       url: '/api/v1/assets/databases/',
-      addFields: [
-        [this.$t('common.Basic'), ['db_name']]
-      ]
+      addFields: this.getAddFields(),
+      addFieldsMeta: {
+        use_ssl: {
+          label: this.$t('common.UseSSL'),
+          component: 'el-switch'
+        },
+        allow_invalid_cert: {
+          label: this.$t('common.AllowInvalidCert'),
+          hidden: (form) => { return !form.use_ssl },
+          component: 'el-switch'
+        },
+        ca_cert: {
+          label: this.$t('common.CACertificate'),
+          hidden: (form) => { return !form.use_ssl },
+          component: UploadKey
+        },
+        client_cert: {
+          label: this.$t('common.ClientCertificate'),
+          hidden: (form) => { return !form.use_ssl },
+          component: UploadKey
+        },
+        client_key: {
+          label: this.$t('common.CertificateKey'),
+          hidden: (form) => { return !form.use_ssl },
+          component: UploadKey
+        }
+      }
+    }
+  },
+  methods: {
+    getAddFields() {
+      const platform = this.$route.query.platform_name
+      const baseFields = [[this.$t('common.Basic'), ['db_name']]]
+      let tlsParams = ['use_ssl', 'ca_cert']
+      switch (platform) {
+        case 'Redis':
+          tlsParams = tlsParams.concat(['client_cert', 'client_key'])
+          break
+        case 'MongoDB':
+          tlsParams = tlsParams.concat(['client_key', 'allow_invalid_cert'])
+          break
+      }
+      if (tlsParams.length > 2) {
+        const secureField = [
+          this.$t('assets.Secure'), tlsParams, 3
+        ]
+        baseFields.push(secureField)
+      }
+      return baseFields
     }
   }
 }
