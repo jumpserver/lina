e741bc8d40d68565a93c2cbda7403d5f4f190b74
Aaron3S <chenyang@fit2cloud.com>
2023-01-11 19:45:49 +0800

    Nan

---
+++ src/views/ops/Template/Playbook/PlaybookDetail/Workspace.vue
@@ -0,0 +1,43 @@
+<template>
+  <TreeTable component="FileTree">
+    <template slot="table">
+      <el-tabs active-name="first">
+        <el-tab-pane label="main.yml" name="first">
+          <CodeEditor />
+        </el-tab-pane>
+      </el-tabs>
+    </template>
+  </TreeTable>
+</template>
+
+<script>
+
+import { TreeTable } from '@/components'
+import CodeEditor from '@/components/FormFields/CodeEditor.vue'
+
+export default {
+  name: 'WorkspaceVue',
+  components: {
+    TreeTable,
+    CodeEditor
+  },
+  props: {
+    object: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {}
+  },
+  mounted() {
+    this.$axios.get(`/api/v1/ops/playbook/${this.object.id}/file/`).then(data => {
+      console.log(data)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
