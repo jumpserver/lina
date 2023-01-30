6ce6fd6c2126da8739de63eb4455925f25105ddf
“huailei000” <2280131253@qq.com>
2023-01-30 15:15:16 +0800

    feat: 资产详情-账号列表添加账号模版

--- src/views/assets/Asset/AssetDetail/Account.vue
+++ src/views/assets/Asset/AssetDetail/Account.vue
@@ -11,6 +11,13 @@
           :has-clone="false"
           :has-left-actions="true"
           :columns="columns"
+          :header-extra-actions="headerExtraActions"
+        />
+        <AccountTemplateDialog
+          v-if="templateDialogVisible"
+          :show-create="false"
+          :visible.sync="templateDialogVisible"
+          @onConfirm="onConfirm"
         />
       </el-col>
     </el-row>
@@ -19,11 +26,13 @@
 
 <script>
 import { AccountListTable } from '@/components'
+import AccountTemplateDialog from '@/views/assets/Asset/AssetCreateUpdate/components/AccountTemplateDialog'
 
 export default {
   name: 'Detail',
   components: {
-    AccountListTable
+    AccountListTable,
+    AccountTemplateDialog
   },
   props: {
     object: {
@@ -37,15 +46,44 @@ export default {
   },
   data() {
     return {
+      templateDialogVisible: false,
       columns: [
         'name', 'username', 'version', 'privileged', 'connectivity',
         'is_active', 'date_created', 'date_updated', 'actions'
+      ],
+      headerExtraActions: [
+        {
+          name: this.$t('route.AccountTemplate'),
+          title: this.$t('route.AccountTemplate'),
+          can: () => this.$hasPerm('accounts.view_accounttemplate'),
+          callback: () => {
+            this.templateDialogVisible = true
+          }
+        }
       ]
     }
   },
   computed: {
     iUrl() {
       return this.url || `/api/v1/accounts/accounts/?asset=${this.object.id}`
+    }
+  },
+  methods: {
+    onConfirm(data) {
+      data = data?.map(i => {
+        if (i.hasOwnProperty('id')) {
+          delete i['id']
+        }
+        i.asset = this.object.id
+        return i
+      })
+      this.$axios.post(`/api/v1/accounts/accounts/`, data).then(() => {
+        this.templateDialogVisible = false
+        this.$refs.ListTable.addAccountSuccess()
+        this.$message.success(this.$tc('common.createSuccessMsg'))
+      }).catch(() => {
+        this.$message.error(this.$tc('common.createErrorMsg'))
+      })
     }
   }
 }
