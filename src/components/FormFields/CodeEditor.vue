9cb97c68ed707e879c7809bb6a20cf21a4f72334
fit2bot <68588906+fit2bot@users.noreply.github.com>
2023-02-03 15:08:54 +0800

    perf: 优化账号策略样式 (#2505)
    Co-authored-by: Aaron3S <chenyang@fit2cloud.com>
    Co-authored-by: Jiangjie.Bai <bugatti_it@163.com>

--- src/components/FormFields/CodeEditor.vue
+++ src/components/FormFields/CodeEditor.vue
@@ -24,9 +24,7 @@
             class="inline-input"
             :fetch-suggestions="item.el.query"
             @select="item.callback(item.value)"
-          >
-            <template slot="prepend">{{ item.name }}</template>
-          </el-autocomplete>
+          />
 
           <div v-if="item.type==='select' && item.el && item.el.create" class="select-content">
             <span class="filter-label">
