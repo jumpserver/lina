71935fd600c4f5f31ec39f6d5b785f805c68e8c5
jiangweidong <weidong.jiang@fit2cloud.com>
2023-02-17 15:26:27 +0800

    fix: 终端管理-终端详情中无法显示activity界面

--- src/router/settings/index.js
+++ src/router/settings/index.js
@@ -93,7 +93,7 @@ export default {
         {
           path: ':id',
           name: 'TerminalDetail',
-          component: () => import('@/views/settings/Terminal/Component/TerminalDetail'),
+          component: () => import('@/views/settings/Terminal/Component/TerminalDetail/index'),
           meta: {
             title: i18n.t('route.Terminal'),
             permissions: ['terminal.view_terminal']
