6eaa21bf66e455f66cfd926666832ca1a66a69ec
feng <1304903146@qq.com>
2023-02-23 16:38:51 +0800

    perf: 账号收集 web资产 字段优化

--- src/views/assets/Asset/AssetList/WebList.vue
+++ src/views/assets/Asset/AssetList/WebList.vue
@@ -18,7 +18,10 @@ export default {
           createRoute: 'WebCreate'
         },
         tableConfig: {
-          columnsExclude: ['username_selector', 'password_selector', 'submit_selector', 'script'],
+          columnsExclude: [
+            'username_selector', 'password_selector', 'submit_selector',
+            'script', 'spec_info', 'info', 'auto_info'
+          ],
           columnsMeta: {
             autofill: {
               width: '100px'
