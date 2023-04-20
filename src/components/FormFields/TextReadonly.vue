74df2dc164dacc32dbddc31b0f55e346eab799ba
“huailei000” <2280131253@qq.com>
2023-04-20 15:10:00 +0800

    perf: 文件加密表单显示文字

---
+++ src/components/FormFields/TextReadonly.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{ value? trueText : falseText }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Boolean],
+      default: () => false
+    },
+    trueText: {
+      type: String,
+      default: function() {
+        return this.$t('common.Yes')
+      }
+    },
+    falseText: {
+      type: String,
+      default: function() {
+        return this.$t('common.No')
+      }
+    }
+
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
