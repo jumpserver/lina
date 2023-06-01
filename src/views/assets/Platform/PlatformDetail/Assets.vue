26bce39b127af97421886c9ec4ade3142557c609
fangfangdong <fangfang.dong@fit2cloud.com>
2023-06-01 18:51:55 +0800

    feat: 资产平台详情添加资产列表页（只显示当前组织下的资产）

---
+++ src/views/assets/Platform/PlatformDetail/Assets.vue
@@ -0,0 +1,42 @@
+<template>
+  <ListTable ref="ListTable" :header-actions="headerActions" :table-config="tableConfig" />
+</template>
+
+<script>
+
+import { ListTable } from '@/components'
+
+export default {
+  components: {
+    ListTable
+  },
+  props: {
+    object: {
+      type: Object,
+      default: () => { }
+    }
+  },
+  data() {
+    const tableUrl = `/api/v1/assets/assets/?platform=${this.object.id}`
+    return {
+      tableConfig: {
+        url: tableUrl,
+        category: 'all'
+      },
+      headerActions: {
+        hasCreate: false,
+        hasRefresh: true,
+        hasExport: false,
+        hasImport: false,
+        hasMoreActions: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.asset-select-dialog>>>.transition-box:first-child {
+  background-color: #f3f3f3;
+}
+</style>
