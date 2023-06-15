17748c56c93d0a76a5de3226519740cc52933528
“huailei000” <2280131253@qq.com>
2023-06-15 10:58:36 +0800

    perf: 优化自定义资产列表不显示操作问题

--- src/views/assets/Asset/AssetList/components/BaseList.vue
+++ src/views/assets/Asset/AssetList/components/BaseList.vue
@@ -192,8 +192,8 @@ export default {
                   can: ({ row }) =>
                     this.$hasPerm('assets.test_assetconnectivity') &&
                     !this.$store.getters.currentOrgIsRoot &&
-                    row['auto_config'].ansible_enabled &&
-                    row['auto_config'].ping_enabled,
+                    row?.auto_config?.ansible_enabled &&
+                    row?.auto_config?.ping_enabled,
                   callback: ({ row }) => {
                     if (row.platform.name === 'Gateway') {
                       this.GatewayVisible = true
