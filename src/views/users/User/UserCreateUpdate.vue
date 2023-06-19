b7d2fadda14ba7a08d65d52681543883af47a1fc
fangfang.dong <fangfang.dong@fit2cloud.com>
2023-06-19 16:46:59 +0800

    fix: 修复创建用户时 先输入密码再选择来源为LDAP 密码项仍可输入未隐藏的BUG

--- src/views/users/User/UserCreateUpdate.vue
+++ src/views/users/User/UserCreateUpdate.vue
@@ -55,10 +55,13 @@ export default {
         password: {
           component: UserPassword,
           hidden: (formValue) => {
-            if (formValue.password_strategy === 'custom') {
+            if (formValue.source !== 'local') {
+              return true
+            }
+            if (formValue.password_strategy === 'custom' || formValue.update_password) {
               return false
             }
-            return !formValue.update_password || formValue.source !== 'local'
+            return true
           },
           el: {
             required: false,
@@ -79,10 +82,13 @@ export default {
             }
           ],
           hidden: (formValue) => {
-            if (formValue.password_strategy === 'custom') {
+            if (formValue.source !== 'local') {
+              return true
+            }
+            if (formValue.password_strategy === 'custom' || formValue.update_password || this.user.can_public_key_auth) {
               return false
             }
-            return !formValue.update_password || !this.user.can_public_key_auth
+            return true
           }
         },
         set_public_key: {
