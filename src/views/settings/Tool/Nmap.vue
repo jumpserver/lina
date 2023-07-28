2f122f7fbe1ddb42a972d9cf37359991d6ad9a97
jiangweidong <weidong.jiang@fit2cloud.com>
2023-07-28 10:42:28 +0800

    feat: 系统工具增加服务器时间及nmap工具 (#3289)

---
+++ src/views/settings/Tool/Nmap.vue
@@ -0,0 +1,37 @@
+<template>
+  <Base v-bind="$data" />
+</template>
+
+<script>
+import rules from '@/components/DataForm/rules'
+import Base from './Base'
+
+export default {
+  name: 'Nmap',
+  components: {
+    Base
+  },
+  data() {
+    return {
+      taskType: 'nmap',
+      fields: [
+        {
+          name: 'dest_ip', label: this.$t('setting.destinationIP'),
+          placeholder: this.$t('setting.destinationIP')
+        },
+        {
+          name: 'dest_port', label: this.$t('setting.testPort'), type: 'multiInput'
+        }
+      ],
+      rules: {
+        dest_ip: rules.Required
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/settings/Tool/Ping.vue
@@ -0,0 +1,31 @@
+<template>
+  <Base v-bind="$data" />
+</template>
+
+<script>
+import rules from '@/components/DataForm/rules'
+import Base from './Base'
+
+export default {
+  name: 'Ping',
+  components: {
+    Base
+  },
+  data() {
+    return {
+      taskType: 'ping',
+      fields: [{
+        name: 'dest_ip', label: this.$t('setting.destinationIP'), placeholder: this.$t('setting.destinationIP')
+      }],
+      rules: {
+        dest_ip: rules.Required
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
