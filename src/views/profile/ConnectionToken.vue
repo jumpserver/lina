6fa80528787f294cd4d536f643066a6eb754c1d6
Bai <baijiangjie@gmail.com>
2023-10-13 14:35:38 +0800

    fix: 修复连接令牌过期按钮被禁用的问题

--- src/views/profile/ConnectionToken.vue
+++ src/views/profile/ConnectionToken.vue
@@ -52,7 +52,7 @@ export default {
                   name: 'Expired',
                   title: this.$t('setting.Expire'),
                   type: 'info',
-                  can: ({ row }) => !row['is_expired'] && this.$hasPerm('authentication.change_connectiontoken'),
+                  can: ({ row }) => !row['is_expired'] && this.$hasPerm('authentication.expire_connectiontoken'),
                   callback: function({ row }) {
                     this.$axios.patch(`${ajaxUrl}${row.id}/expire/`,
                     ).then(res => {
