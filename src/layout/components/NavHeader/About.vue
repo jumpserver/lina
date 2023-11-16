5ce5873ba07a1aefdd63c48dd10cf9ac4a2b7923
fit2bot <robot@jumpserver.org>
2023-11-16 20:49:52 +0800

    feat: Update v3.9.0

--- src/layout/components/NavHeader/About.vue
+++ src/layout/components/NavHeader/About.vue
@@ -81,7 +81,7 @@ export default {
         },
         {
           label: this.$t('common.Version'),
-          value: 'version-dev'
+          value: 'v3.9.0'
         },
         {
           label: this.$t('common.PermissionCompany'),
