f3f5bbe114768026b535266d8cacb9623b2ca5d0
fit2bot <68588906+fit2bot@users.noreply.github.com>
2023-11-28 19:07:08 +0800

    feat: 用户详情展示所有会话 (#3563)
    Co-authored-by: wangruidong <940853815@qq.com>

---
+++ src/views/users/User/UserDetail/UserSession.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-row :gutter="10">
+    <el-col :span="18">
+      <OfflineList :url="url" />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import OfflineList from '@/views/sessions/SessionList/OfflineList.vue'
+
+export default {
+  name: 'UserSession',
+  components: {
+    OfflineList
+  },
+  props: {
+    object: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      url: `/api/v1/terminal/sessions/?user_id=${this.object.id}&order=-date_end&is_finished=1`
+    }
+  }
+
+}
+</script>
