5cefbbfb517efc97bd25a941cecc2b67638741d7
ibuler <ibuler@qq.com>
2023-12-18 17:55:46 +0800

    perf: 修改 labels 关联

--- src/views/users/User/UserCreateUpdate.vue
+++ src/views/users/User/UserCreateUpdate.vue
@@ -120,10 +120,7 @@ export default {
             ajax: {
               url: '/api/v1/rbac/system-roles/?id!=00000000-0000-0000-0000-000000000004',
               transformOption: (item) => {
-                if (item.name !== 'SystemComponent') {
-                  return { label: item.display_name, value: item.id }
-                }
-                return false
+                return { label: item.display_name, value: item.id }
               }
             }
           },
