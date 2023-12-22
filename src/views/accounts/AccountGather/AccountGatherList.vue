f6f8301ad56f406c23f220f444f1799000826816
huailei <31801270+huailei000@users.noreply.github.com>
2023-12-22 15:25:13 +0800

    Revert "perf: 账号收集翻译"
    This reverts commit 9a63ae63d47ee8607b3f4d7e278adaa16a922778.

--- src/views/accounts/AccountGather/AccountGatherList.vue
+++ src/views/accounts/AccountGather/AccountGatherList.vue
@@ -146,7 +146,7 @@ export default {
           },
           {
             name: 'BulkSyncDelete',
-            title: this.$t('accounts.BulkSyncDelete'),
+            title: this.$t('common.BulkSyncDelete'),
             type: 'primary',
             icon: 'fa fa-exchange',
             can: ({ selectedRows }) => {
