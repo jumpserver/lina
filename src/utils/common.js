fb0e89c788433d8090b32f085c901a21657ba1ff
wangruidong <940853815@qq.com>
2024-01-15 19:24:22 +0800

    fix: capitalizeFirst error

--- src/utils/common.js
+++ src/utils/common.js
@@ -417,7 +417,7 @@ export function formatFileSize(bytes) {
 
 export function capitalizeFirst(string) {
   if (!string) return string
-  return string.split(' ').map(item => {
+  return string.trim().split(' ').map(item => {
     return item[0].toUpperCase() + item.slice(1)
   }).join(' ')
 }
