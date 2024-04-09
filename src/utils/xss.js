61bb97efa9445302c5e57136a3161b349141bbb1
ibuler <ibuler@qq.com>
2024-04-09 17:00:01 +0800

    perf: xss attack

--- src/utils/xss.js
+++ src/utils/xss.js
@@ -1,22 +1,22 @@
 const xss = require('xss')
+const excludeTags = ['iframe', 'script']
 
 const options = {
   css: false,
   stripIgnoreTagBody: ['script'],
   onTag(tag, html, options) {
-    if (tag === 'iframe') {
-      return html.replace(/javascript:?/, '')
+    if (excludeTags.indexOf(tag) !== -1) {
+      return html.replace(/</g, '&lt;').replace(/>/g, '&gt;')
     }
   },
   // 避免把页面样式过滤掉
-  onIgnoreTagAttr(tag, name, value, isWhiteAttr) {
+  onTagAttr(tag, name, value, isWhiteAttr) {
     // 过滤掉标签上的事件
-    if (/^[^on]/.test(name)) {
-      return name + '="' + xss.escapeAttrValue(value) + '"'
-    } else {
-      if (tag === 'a') {
-        return name + '="' + xss.escapeAttrValue(value) + '"'
-      }
+    if (/^on/.test(name)) {
+      return name + '=' + '.'
+    }
+    if (['src', 'href'].indexOf(name) !== -1) {
+      return name + '=' + value.replace('javascript:', 'java:').replace('data:', 'dt:')
     }
   }
 }
