7a138388b2ba9334607307c5fff508e0ba700567
zhaojisen <1301338853@qq.com>
2024-11-19 18:04:28 +0800

    fixed: Fixes ESLint issues

--- src/views/settings/Terminal/Endpoint/EndpointList.vue
+++ src/views/settings/Terminal/Endpoint/EndpointList.vue
@@ -23,7 +23,7 @@ export default {
           min: ['name', 'actions'],
           default: [
             'name', 'host', 'actions',
-            'http_port', 'https_port', 'ssh_port', 'rdp_port','vnc_port',
+            'http_port', 'https_port', 'ssh_port', 'rdp_port', 'vnc_port',
             'mysql_port', 'mariadb_port', 'postgresql_port',
             'redis_port', 'sqlserver_port', 'oracle_port_range', 'is_active'
           ]
