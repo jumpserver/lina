1e37ecff11cf391f511e6f9dacc64e51a3dd738b
wangruidong <940853815@qq.com>
2024-11-26 14:43:31 +0800

    perf: Add variable management to Job and Adhoc details

--- src/views/ops/Template/Adhoc/AdhocDetail/AdhocDetail.vue
+++ src/views/ops/Template/Adhoc/AdhocDetail/AdhocDetail.vue
@@ -3,14 +3,27 @@
     <el-col :md="16" :sm="24">
       <AutoDetailCard :excludes="excludes" :object="object" :url="url" />
     </el-col>
+    <el-col :md="8" :sm="24">
+      <IBox :title="$tc('Variable')">
+        <Variable
+          :value.sync="object.variable"
+          :disable-edit="disableEdit"
+          @input="updateVariable"
+        />
+      </IBox>
+    </el-col>
   </el-row>
 </template>
 
 <script type="text/jsx">
 import AutoDetailCard from '@/components/Cards/DetailCard/auto'
+import Variable from '@/views/ops/Template/components/Variable'
+import { IBox } from '@/components'
 
 export default {
   components: {
+    IBox,
+    Variable,
     AutoDetailCard
   },
   props: {
@@ -22,7 +35,8 @@ export default {
   data() {
     return {
       url: `/api/v1/ops/adhocs/${this.object.id}/`,
-      excludes: ['variable']
+      excludes: ['variable'],
+      disableEdit: this.object.creator !== this.$store.state.users.profile.id
     }
   },
   computed: {
@@ -30,7 +44,14 @@ export default {
       return this.object.name
     }
   },
-  methods: {}
+  methods: {
+    updateVariable() {
+      const url = `/api/v1/ops/adhocs/${this.object.id}/`
+      this.$axios.patch(url, { variable: this.object.variable }).then(() => {
+        this.$message.success(this.$tc('UpdateSuccessMsg'))
+      })
+    }
+  }
 }
 </script>
 
