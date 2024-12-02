688f06ebac2490f4959f3d59f045141eb4952ee5
zhaojisen <1301338853@qq.com>
2024-12-02 14:21:15 +0800

    style: Adjust style

--- src/views/dashboard/ChangeSecret/index.vue
+++ src/views/dashboard/ChangeSecret/index.vue
@@ -19,13 +19,15 @@
       </el-row>
 
       <el-row type="flex" :gutter="24">
-        <FailedAccountSummary :days="days" />
-      </el-row>
 
-      <el-row type="flex" style="flex-direction: column">
-        <el-col :span="24">
+        <el-col :span="14">
+          <FailedAccountSummary :days="days" class="failed-account-summary" />
+        </el-col>
+
+        <el-col :span="10">
           <AccountSummary class="account-summary" :days="days" />
         </el-col>
+
       </el-row>
 
     </div>
@@ -70,6 +72,16 @@ export default {
 
   .page ::v-deep .page-heading {
     display: none;
+  }
+
+  .failed-account-summary {
+    height: 100%;
+    margin-top: 16px;
+
+   ::v-deep #HomeCard .el-card.no-border {
+     height: 100%;
+     margin-bottom: unset !important;
+    }
   }
 
 </style>
