77b455594faec9bf0583aaa1a0d9847e47146ac2
wangruidong <940853815@qq.com>
2024-12-12 15:56:04 +0800

    perf: Add AccountDetail page

---
+++ src/views/accounts/AccountDiscover/AccountDetail/more.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :md="16" :sm="24">
+      <AutoDetailCard :object="detail" :url="url" />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import AutoDetailCard from '@/components/Cards/DetailCard/auto.vue'
+
+export default {
+  components: {
+    AutoDetailCard
+  },
+  props: {
+    object: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      url: `/api/v1/accounts/gathered-accounts/${this.object.id}/details/`,
+      detail: this.object?.detail
+    }
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
