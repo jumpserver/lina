a9ba931bba7a3781c25fea66fd6ad473470f6c10
wangruidong <940853815@qq.com>
2024-12-18 10:53:12 +0800

    perf: Add 'labels' column to AssetPermission and Domain tables

--- src/views/perms/AssetPermission/AssetPermissionList.vue
+++ src/views/perms/AssetPermission/AssetPermissionList.vue
@@ -50,7 +50,7 @@ export default {
         columnsExtra: ['action'],
         columns: [
           'name', 'users_amount', 'user_groups_amount', 'assets_amount', 'nodes_amount',
-          'accounts', 'is_valid', 'is_expired', 'from_ticket', 'is_active', 'actions'
+          'accounts', 'labels', 'is_valid', 'is_expired', 'from_ticket', 'is_active', 'actions'
         ],
         columnsShow: {
           min: ['name', 'actions'],
