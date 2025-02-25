8e461e8aede09e9a319d8d1db1e74634196f8be9
ibuler <ibuler@qq.com>
2025-02-25 16:55:15 +0800

    perf: update quick filter expand setting

--- src/views/pam/Account/index.vue
+++ src/views/pam/Account/index.vue
@@ -24,13 +24,13 @@ export default {
         submenu: [
           {
             name: 'account',
-            title: this.$t('Account'),
+            title: this.$t('Accounts'),
             icon: 'fa-key',
             component: () => import('@/views/pam/Account/AccountList.vue')
           },
           {
             name: 'asset',
-            title: this.$t('Asset'),
+            title: this.$t('Assets'),
             icon: 'fa-inbox',
             component: () => import('@/views/pam/Account/AssetList.vue')
           },
