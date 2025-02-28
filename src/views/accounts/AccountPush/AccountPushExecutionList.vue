4c46db0787329cc1b302fccc2873857ee5191aba
zhaojisen <1301338853@qq.com>
2025-02-28 14:28:44 +0800

    Fixed: Risk List Detail Drawer

--- src/views/accounts/AccountPush/AccountPushExecutionList.vue
+++ src/views/accounts/AccountPush/AccountPushExecutionList.vue
@@ -50,7 +50,7 @@ export default {
                 name: 'AccountPushExecutionDetail',
                 params: { id: row.id }
               }),
-              getTitle: ({ row }) => row.automation.slice(0, 8),
+              getTitle: ({ row }) => row.id.slice(0, 8),
               drawer: true,
               can: this.$hasPerm('accounts.view_pushaccountexecution')
             }
