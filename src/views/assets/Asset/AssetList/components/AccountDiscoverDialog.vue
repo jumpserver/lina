5218ef2ca7783caa530274fc01bf0a06313ee774
w940853815 <940853815@qq.com>
2025-03-13 14:24:27 +0800

    perf: Translate

--- src/views/assets/Asset/AssetList/components/AccountDiscoverDialog.vue
+++ src/views/assets/Asset/AssetList/components/AccountDiscoverDialog.vue
@@ -4,7 +4,7 @@
       :destroy-on-close="true"
       :show-cancel="false"
       :show-confirm="false"
-      :title="$tc('DiscoverAccountsDialog')"
+      :title="$tc('AccountDiscover')"
       :visible.sync="iVisible"
       top="35vh"
       width="80%"
