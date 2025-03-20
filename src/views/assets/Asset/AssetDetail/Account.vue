2dec820a0a81d411ad7a29cad41404e912e5c987
ibuler <ibuler@qq.com>
2025-03-20 18:13:01 +0800

    perf: add default conn btn

--- src/views/assets/Asset/AssetDetail/Account.vue
+++ src/views/assets/Asset/AssetDetail/Account.vue
@@ -59,7 +59,7 @@ export default {
     return {
       title: this.$t('Test'),
       templateDialogVisible: false,
-      columnsDefault: ['name', 'username', 'asset'],
+      columnsDefault: ['name', 'username', 'asset', 'connect'],
       headerExtraActions: [
         {
           name: this.$t('AccountTemplate'),
