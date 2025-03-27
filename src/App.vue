87ebd8c08b2d36c25f93abe1d1b265f53e978e77
zhaojisen <1301338853@qq.com>
2025-03-27 17:11:15 +0800

    Perf: Add Lina Watermark

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,52 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
+import { Watermark } from 'watermark-js-plus'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      watermark: null
+    }
+  },
   computed: {
     ...mapState({
       isRouterAlive: state => state.common.isRouterAlive
+    }),
+    ...mapGetters({
+      currentUser: 'currentUser',
+      publicSettings: 'publicSettings'
     })
+  },
+  watch: {
+    currentUser: {
+      handler(newVal) {
+        this.createWatermark()
+      }
+    },
+    'publicSettings.SECURITY_WATERMARK_ENABLED': {
+      handler(newVal) {
+        this.createWatermark()
+      }
+    }
+  },
+  methods: {
+    createWatermark() {
+      if (this.currentUser?.username && this.publicSettings?.SECURITY_WATERMARK_ENABLED) {
+        this.watermark = new Watermark({
+          content: this.currentUser.username,
+          width: 200,
+          height: 200,
+          rotate: 45,
+          fontWeight: 'normal',
+          fontColor: 'rgba(128, 128, 128, 0.3)'
+        })
+
+        this.watermark.create()
+      }
+    }
   }
 }
 </script>
