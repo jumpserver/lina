5c4aa457fad9000fa690b3aebab412b25e765365
w940853815 <940853815@qq.com>
2025-04-29 10:15:35 +0800

    perf: Support watermark customization

--- src/views/settings/Security/Session.vue
+++ src/views/settings/Security/Session.vue
@@ -1,18 +1,21 @@
 <template>
   <IBox>
     <GenericCreateUpdateForm submit-method="patch" v-bind="config" />
+    <WatermarkHelpDialog :visible.sync="showHelpDialog" />
   </IBox>
 </template>
 
 <script>
 import IBox from '@/components/Common/IBox/index.vue'
 import GenericCreateUpdateForm from '@/layout/components/GenericCreateUpdateForm/index.vue'
+import WatermarkHelpDialog from './WatermarkHelpDialog.vue'
 
 export default {
   name: 'SessionSecurity',
-  components: { GenericCreateUpdateForm, IBox },
+  components: { GenericCreateUpdateForm, IBox, WatermarkHelpDialog },
   data() {
     return {
+      showHelpDialog: false,
       config: {
         url: '/api/v1/settings/setting/?category=security_session',
         hasDetailInMsg: false,
@@ -20,16 +23,66 @@ export default {
           [
             this.$t('Basic'),
             [
-              'SECURITY_WATERMARK_ENABLED',
               'SECURITY_SESSION_SHARE',
               'SESSION_EXPIRE_AT_BROWSER_CLOSE',
               'VIEW_ASSET_ONLINE_SESSION_INFO',
               'SECURITY_MAX_IDLE_TIME',
               'SECURITY_MAX_SESSION_TIME'
             ]
+          ],
+          [
+            this.$t('Watermark'),
+            this.$store.getters.hasValidLicense ? [
+              'SECURITY_WATERMARK_ENABLED',
+              'SECURITY_WATERMARK_SESSION_CONTENT',
+              'SECURITY_WATERMARK_CONSOLE_CONTENT',
+              'SECURITY_WATERMARK_COLOR',
+              'SECURITY_WATERMARK_FONT_SIZE',
+              'SECURITY_WATERMARK_HEIGHT',
+              'SECURITY_WATERMARK_WIDTH',
+              'SECURITY_WATERMARK_ROTATE'
+            ] : []
           ]
         ],
-        fieldsMeta: {},
+        fieldsMeta: {
+          SECURITY_WATERMARK_SESSION_CONTENT: {
+            helpTextFormatter: () => {
+              const handleClick = () => {
+                this.showHelpDialog = true
+              }
+              return (
+                <i onClick={handleClick} class='fa fa-external-link'>{this.$t('Help')}</i>
+              )
+            }
+          },
+          SECURITY_WATERMARK_CONSOLE_CONTENT: {
+            helpTextFormatter: () => {
+              const handleClick = () => {
+                this.showHelpDialog = true
+              }
+              return (
+                <i onClick={handleClick} class='fa fa-external-link'>{this.$t('Help')}</i>
+              )
+            }
+          },
+          SECURITY_WATERMARK_COLOR: {
+            component: 'el-color-picker',
+            el: {
+              size: 'small',
+              'show-alpha': true,
+              predefine: [
+                'rgba(128, 128, 128, 0.25)',
+                'rgba(0, 0, 0, 0.15)',
+                'rgba(255, 255, 255, 0.2)',
+                'rgba(176, 176, 176, 0.3)',
+                'rgba(91, 155, 213, 0.25)',
+                'rgba(192, 80, 77, 0.2)',
+                'rgba(112, 173, 71, 0.2)',
+                'rgba(255, 192, 0, 0.18)'
+              ]
+            }
+          }
+        },
         onSubmit: async(validValues) => {
           const url = '/api/v1/settings/setting/?category=security_session'
 
