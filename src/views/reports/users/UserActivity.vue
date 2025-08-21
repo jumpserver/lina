e3b4d43c76a823b42fa331ef68459bc4b4ed0e77
feng <1304903146@qq.com>
2025-08-21 11:35:27 +0800

    perf: echars pie fontSize 15

--- src/views/reports/users/UserActivity.vue
+++ src/views/reports/users/UserActivity.vue
@@ -191,7 +191,7 @@ export default {
             emphasis: {
               label: {
                 show: true,
-                fontSize: 20,
+                fontSize: 15,
                 fontWeight: 'bold'
               }
             },
