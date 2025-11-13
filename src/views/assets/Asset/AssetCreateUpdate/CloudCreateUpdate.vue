0c85e0c6838b055015769b4721b2d87e11dc5791
Ewall555 <a03216@foxmail.com>
2025-11-13 17:00:03 +0800

    feat: Add fields and metadata for Kubernetes platform in CloudCreateUpdate component

--- src/views/assets/Asset/AssetCreateUpdate/CloudCreateUpdate.vue
+++ src/views/assets/Asset/AssetCreateUpdate/CloudCreateUpdate.vue
@@ -10,7 +10,33 @@ export default {
   components: { BaseAssetCreateUpdate },
   data() {
     return {
-      url: '/api/v1/assets/clouds/'
+      url: '/api/v1/assets/clouds/',
+      addFields: this.getAddFields(),
+      addFieldsMeta: this.getAddFieldsMeta()
+    }
+  },
+  methods: {
+    getAddFields() {
+      const platform = this.$route.query.type
+      const baseFields = []
+
+      if (platform === 'k8s') {
+        baseFields.push([this.$t('Basic'), ['namespace'], 1])
+      }
+
+      return baseFields
+    },
+    getAddFieldsMeta() {
+      const platform = this.$route.query.type
+      const fieldsMeta = {}
+
+      if (platform === 'k8s') {
+        fieldsMeta['namespace'] = {
+          label: this.$t('DefaultNamespace')
+        }
+      }
+
+      return fieldsMeta
     }
   }
 }
