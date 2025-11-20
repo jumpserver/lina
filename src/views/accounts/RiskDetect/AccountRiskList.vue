20e1c833a6ba929fdb34f460f621e1fd2145dc70
w940853815 <940853815@qq.com>
2025-11-20 10:29:30 +0800

    perf: Update asset detection logic in AccountRiskList for improved menu display

--- src/views/accounts/RiskDetect/AccountRiskList.vue
+++ src/views/accounts/RiskDetect/AccountRiskList.vue
@@ -43,7 +43,9 @@ export default {
         asset: ''
       },
       treeSetting: {
-        showMenu: true,
+        showMenu: (node) => {
+          return node?.meta?.type === 'asset'
+        },
         showRefresh: true,
         showSearch: true,
         showAssets: true,
@@ -55,9 +57,8 @@ export default {
         menu: [
           {
             id: 'check',
-            name: this.$t('Check'),
+            name: this.$t('RiskDetection'),
             icon: 'scan',
-            has: (node) => node?.meta?.type === 'asset',
             callback: (node) => {
               vm.detectDialog.asset = node.id
               setTimeout(() => {
