af1b477d2f55aaa148c03e73b81ea2a5daec5ca3
Bai <baijiangjie@gmail.com>
2026-04-10 19:57:36 +0800

    perf: AttrRulesFilterBackend support filter by attr_rules_instance, resolve detail page url params too long

--- src/utils/jms/index.js
+++ src/utils/jms/index.js
@@ -178,6 +178,17 @@ export function toM2MJsonParams(attrFilter) {
   return ['attr_rules', encodeURIComponent(btoa(String.fromCharCode(...data)))]
 }
 
+export function toM2MInstanceJsonParams(instanceAppModel, instanceId) {
+  const encoder = new TextEncoder()
+  const [instanceApp, instanceModel] = instanceAppModel.split('.')
+  const data = encoder.encode(JSON.stringify({
+    'app': instanceApp,
+    'model': instanceModel,
+    'id': instanceId
+  }))
+  return ['attr_rules_instance', encodeURIComponent(btoa(String.fromCharCode(...data)))]
+}
+
 export function IsSupportPauseSessionType(terminalType) {
   const supportedType = ['koko', 'lion', 'chen', 'kael']
   return supportedType.includes(terminalType)
