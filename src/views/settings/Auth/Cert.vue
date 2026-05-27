686f09ca28e37f3183b15dd16192b39be040d1f4
Bai <baijiangjie@gmail.com>
2026-05-27 14:33:53 +0800

    perf: add config for cert

--- src/views/settings/Auth/Cert.vue
+++ src/views/settings/Auth/Cert.vue
@@ -8,6 +8,8 @@
 
 <script>
 import BaseAuth from './Base'
+import { UploadKey } from '@/components'
+import TextReadonly from '@/components/Form/FormFields/TextReadonly.vue'
 
 export default {
   name: 'Cert',
@@ -19,13 +21,42 @@ export default {
       settings: {
         url: '/api/v1/settings/setting/?category=cert',
         fields: [
-          'AUTH_CERT',
-          'AUTH_CERT_ENROLL_ENABLED',
-          'AUTH_CERT_ENROLL_VALIDITY_DAYS',
-          'AUTH_CERT_CHALLENGE_TTL',
-          'AUTH_CERT_DEFAULT_PIN'
+          [this.$t('Basic'), [
+            'AUTH_CERT',
+            'AUTH_CERT_CHALLENGE_TTL',
+            'AUTH_CERT_DEFAULT_PIN'
+          ]],
+          [this.$t('Enrollment'), [
+            'AUTH_CERT_ENROLL_ENABLED',
+            'AUTH_CERT_ENROLL_VALIDITY_DAYS',
+            'AUTH_CERT_CA_CERT_CONTENT',
+            'AUTH_CERT_CA_CERT_ALGORITHM',
+            'AUTH_CERT_CA_KEY_CONTENT',
+            'AUTH_CERT_CA_KEY_PASS'
+          ]]
         ],
         fieldsMeta: {
+          AUTH_CERT_ENROLL_VALIDITY_DAYS: {
+            hidden: (form) => !form['AUTH_CERT_ENROLL_ENABLED']
+          },
+          AUTH_CERT_CA_CERT_CONTENT: {
+            component: UploadKey,
+            hidden: (form) => !form['AUTH_CERT_ENROLL_ENABLED']
+          },
+          AUTH_CERT_CA_CERT_ALGORITHM: {
+            component: TextReadonly,
+            el: {
+              bolder: false
+            },
+            hidden: (form) => !form['AUTH_CERT_ENROLL_ENABLED']
+          },
+          AUTH_CERT_CA_KEY_CONTENT: {
+            component: UploadKey,
+            hidden: (form) => !form['AUTH_CERT_ENROLL_ENABLED']
+          },
+          AUTH_CERT_CA_KEY_PASS: {
+            hidden: (form) => !form['AUTH_CERT_ENROLL_ENABLED']
+          }
         },
         submitMethod: () => 'patch',
         afterGetFormValue(obj) {
