0a77c9428916ea2cda937279e16d55a8552c8434
w940853815 <940853815@qq.com>
2026-06-05 18:57:16 +0800

    feat: Support LDAP/LDAPS to upload CA/ client certificates and enable StartTLS.

--- src/views/settings/Auth/Ldap/LdapHA.vue
+++ src/views/settings/Auth/Ldap/LdapHA.vue
@@ -1,13 +1,9 @@
 <template>
   <IBox>
     <GenericCreateUpdateForm v-bind="$data" />
-    <ImportDialog
-      v-if="dialogLdapUserImport"
-      v-model:visible="dialogLdapUserImport"
-      :category="category"
-    />
-    <TestLoginDialog v-model:visible="dialogTest" :category="category" />
-    <SyncSettingDialog v-model:visible="dialogSyncSetting" />
+    <ImportDialog v-if="dialogLdapUserImport" :category="category" :visible.sync="dialogLdapUserImport" />
+    <TestLoginDialog :visible.sync="dialogTest" :category="category" />
+    <SyncSettingDialog :visible.sync="dialogSyncSetting" />
   </IBox>
 </template>
 <script>
@@ -15,7 +11,7 @@ import GenericCreateUpdateForm from '@/layout/components/GenericCreateUpdateForm
 import ImportDialog from './ImportDialog.vue'
 import TestLoginDialog from './TestLoginDialog.vue'
 import SyncSettingDialog from './HaSyncSettingDialog.vue'
-import { IBox } from '@/components'
+import { IBox, UploadKey } from '@/components'
 import rules, { JsonRequired } from '@/components/Form/DataForm/rules'
 import { JsonEditor, UpdateToken } from '@/components/Form/FormFields'
 
@@ -36,45 +32,66 @@ export default {
       dialogTest: false,
       dialogLdapUserImport: false,
       dialogSyncSetting: false,
-      encryptedFields: ['AUTH_LDAP_HA_BIND_PASSWORD'],
+      encryptedFields: [
+        'AUTH_LDAP_HA_BIND_PASSWORD',
+        'AUTH_LDAP_HA_CACERT_CONTENT',
+        'AUTH_LDAP_HA_CERT_CONTENT',
+        'AUTH_LDAP_HA_KEY_CONTENT'
+      ],
       fields: [
         [
           this.$t('Basic'),
           [
-            'AUTH_LDAP_HA',
-            'AUTH_LDAP_HA_SERVER_URI',
-            'AUTH_LDAP_HA_BIND_DN',
-            'AUTH_LDAP_HA_BIND_PASSWORD'
+            'AUTH_LDAP_HA', 'AUTH_LDAP_HA_SERVER_URI',
+            'AUTH_LDAP_HA_BIND_DN', 'AUTH_LDAP_HA_BIND_PASSWORD',
+            'AUTH_LDAP_HA_START_TLS',
+            'AUTH_LDAP_HA_CACERT_CONTENT', 'AUTH_LDAP_HA_CERT_CONTENT', 'AUTH_LDAP_HA_KEY_CONTENT'
           ]
         ],
         [
           this.$t('Search'),
-          ['AUTH_LDAP_HA_SEARCH_OU', 'AUTH_LDAP_HA_SEARCH_FILTER', 'AUTH_LDAP_HA_USER_ATTR_MAP']
+          [
+            'AUTH_LDAP_HA_SEARCH_OU', 'AUTH_LDAP_HA_SEARCH_FILTER',
+            'AUTH_LDAP_HA_USER_ATTR_MAP'
+          ]
         ],
         [
           this.$t('Other'),
           [
-            'AUTH_LDAP_HA_STRICT_SYNC',
-            'AUTH_LDAP_HA_CONNECT_TIMEOUT',
-            'AUTH_LDAP_HA_SEARCH_PAGED_SIZE',
-            'AUTH_LDAP_HA_CACHE_TIMEOUT',
-            'AUTH_LDAP_HA_ALWAYS_UPDATE_USER'
+            'AUTH_LDAP_HA_STRICT_SYNC', 'AUTH_LDAP_HA_CONNECT_TIMEOUT', 'AUTH_LDAP_HA_SEARCH_PAGED_SIZE',
+            'AUTH_LDAP_HA_CACHE_TIMEOUT'
           ]
         ]
       ],
       fieldsMeta: {
         AUTH_LDAP_HA_BIND_DN: {
-          rules: [rules.Required]
+          rules: [
+            rules.Required
+          ]
         },
         AUTH_LDAP_HA_BIND_PASSWORD: {
           component: UpdateToken
         },
         AUTH_LDAP_HA_SEARCH_OU: {
-          rules: [rules.Required]
+          rules: [
+            rules.Required
+          ]
         },
         AUTH_LDAP_HA_USER_ATTR_MAP: {
           component: JsonEditor,
           rules: [JsonRequired]
+        },
+        AUTH_LDAP_HA_CACERT_CONTENT: {
+          component: UploadKey,
+          helpText: this.$t('AuthLdapCACertHelpText')
+        },
+        AUTH_LDAP_HA_CERT_CONTENT: {
+          component: UploadKey,
+          helpText: this.$t('AuthLdapCertHelpText')
+        },
+        AUTH_LDAP_HA_KEY_CONTENT: {
+          component: UploadKey,
+          helpText: this.$t('AuthLdapKeyHelpText')
         }
       },
       hasDetailInMsg: false,
@@ -82,10 +99,15 @@ export default {
         {
           title: this.$t('LdapConnectTest'),
           loading: false,
-          callback: function (value, form, btn) {
+          callback: function(value, form, btn) {
             if (value['AUTH_LDAP_HA_BIND_PASSWORD'] === undefined) {
               value['AUTH_LDAP_HA_BIND_PASSWORD'] = ''
             }
+            ['AUTH_LDAP_HA_CACERT_CONTENT', 'AUTH_LDAP_HA_CERT_CONTENT', 'AUTH_LDAP_HA_KEY_CONTENT'].forEach((key) => {
+              if (value[key] === undefined) {
+                value[key] = ''
+              }
+            })
             btn.loading = true
             this.enableWS()
             this.ws.onopen = (e) => {
@@ -104,19 +126,19 @@ export default {
         },
         {
           title: this.$t('LdapLoginTest'),
-          callback: function (value, form) {
+          callback: function(value, form) {
             this.dialogTest = true
           }.bind(this)
         },
         {
           title: this.$t('LdapBulkImport'),
-          callback: function (value, form) {
+          callback: function(value, form) {
             this.dialogLdapUserImport = true
           }.bind(this)
         },
         {
           title: this.$t('SyncSetting'),
-          callback: function (value, form) {
+          callback: function(value, form) {
             this.dialogSyncSetting = true
           }.bind(this)
         }
@@ -129,6 +151,11 @@ export default {
         if (data['AUTH_LDAP_HA_BIND_PASSWORD'] === '') {
           delete data['AUTH_LDAP_HA_BIND_PASSWORD']
         }
+        ['AUTH_LDAP_HA_CACERT_CONTENT', 'AUTH_LDAP_HA_CERT_CONTENT', 'AUTH_LDAP_HA_KEY_CONTENT'].forEach((key) => {
+          if (data[key] === '') {
+            delete data[key]
+          }
+        })
         return data
       }
     }
@@ -146,7 +173,8 @@ export default {
 </script>
 
 <style scoped>
-.listTable :deep(.table-action-right-side) {
+.listTable ::v-deep .table-action-right-side {
   padding-top: 0 !important;
 }
+
 </style>
