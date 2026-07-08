48c72404935457b77fb4ef5e35103748905fbca1
ibuler <ibuler@qq.com>
2026-07-08 17:10:44 +0800

    perf: update block ip

--- src/components/Apps/BlockedIPs/BlockedIPList.vue
+++ src/components/Apps/BlockedIPs/BlockedIPList.vue
@@ -1,31 +1,36 @@
 <template>
-  <ListTable ref="ListTable" :header-actions="headerActions" :table-config="tableConfig" />
+  <ListTable
+    ref="ListTable"
+    :header-actions="headerActions"
+    :table-config="tableConfig"
+  />
 </template>
 
 <script>
-import { DrawerListTable as ListTable } from '@/components'
+import { DrawerListTable as ListTable } from "@/components";
 
 export default {
-  name: 'BlockedIPList',
+  name: "BlockedIPList",
   components: {
-    ListTable
+    ListTable,
   },
   props: {
     object: {
       type: Object,
       required: false,
-      default: () => ({})
-    }
+      default: () => ({}),
+    },
   },
   data() {
-    const vm = this
+    const vm = this;
     return {
       tableConfig: {
-        url: '/api/v1/settings/security/block-ip/',
-        columns: ['ip', 'actions'],
+        url: "/api/v1/settings/security/block-ip/",
+        columns: ["ip", "actions"],
+        totalData: [],
         columnsMeta: {
           ip: {
-            label: this.$t('IP')
+            label: this.$t("IP"),
           },
           actions: {
             formatterArgs: {
@@ -34,29 +39,31 @@ export default {
               hasClone: false,
               extraActions: [
                 {
-                  name: 'UnlockIP',
-                  title: this.$t('Unblock'),
-                  can: this.$hasPerm('settings.change_security'),
-                  type: 'primary',
+                  name: "UnlockIP",
+                  title: this.$t("Unblock"),
+                  can: this.$hasPerm("settings.change_security"),
+                  type: "primary",
                   callback: ({ row }) => {
                     this.$axios
-                      .post('/api/v1/settings/security/unlock-ip/', { ips: [row.ip] })
-                      .then(() => {
-                        vm.$message.success(this.$tc('UnlockSuccessMsg'))
-                        vm.$refs.ListTable.reloadTable()
+                      .post("/api/v1/settings/security/unlock-ip/", {
+                        ips: [row.ip],
                       })
-                  }
-                }
-              ]
-            }
-          }
-        }
+                      .then(() => {
+                        vm.$message.success(this.$tc("UnlockSuccessMsg"));
+                        vm.$refs.ListTable.reloadTable();
+                      });
+                  },
+                },
+              ],
+            },
+          },
+        },
       },
       headerActions: {
         hasExport: false,
         hasImport: false,
         hasCreate: false,
-        hasSearch: false,
+        hasSearch: true,
         hasRefresh: true,
         hasBulkDelete: false,
         hasBulkUpdate: false,
@@ -64,30 +71,35 @@ export default {
         hasRightActions: true,
         extraMoreActions: [
           {
-            name: 'UnlockSelected',
-            title: this.$t('UnblockSelected'),
-            type: 'primary',
+            name: "UnlockSelected",
+            title: this.$t("UnblockSelected"),
+            type: "primary",
             can: ({ selectedRows }) => {
-              return selectedRows.length > 0
+              return selectedRows.length > 0;
             },
             callback: function ({ selectedRows }) {
               vm.$axios
-                .post('/api/v1/settings/security/unlock-ip/', {
+                .post("/api/v1/settings/security/unlock-ip/", {
                   ips: selectedRows.map((v) => {
-                    return v.ip
-                  })
+                    return v.ip;
+                  }),
                 })
                 .then((res) => {
-                  vm.$message.success(vm.$tc('UnlockSuccessMsg'))
-                  vm.$refs.ListTable.reloadTable()
-                })
-            }
-          }
-        ]
-      }
-    }
-  }
-}
+                  vm.$message.success(vm.$tc("UnlockSuccessMsg"));
+                  vm.$refs.ListTable.reloadTable();
+                });
+            },
+          },
+        ],
+      },
+    };
+  },
+  mounted() {
+    this.$axios.get("/api/v1/settings/security/block-ip/").then((res) => {
+      this.tableConfig.totalData = res.results;
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
