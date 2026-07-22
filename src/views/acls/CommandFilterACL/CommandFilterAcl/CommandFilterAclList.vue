7a6f8f5aa8c460ef6de0fb5e277de19f154a75cb
Bai <baijiangjie@gmail.com>
2026-07-22 18:22:25 +0800

    perf: cmdfilter acl cmdgroup-amount

--- src/views/acls/CommandFilterACL/CommandFilterAcl/CommandFilterAclList.vue
+++ src/views/acls/CommandFilterACL/CommandFilterAcl/CommandFilterAclList.vue
@@ -31,7 +31,7 @@ export default {
         columnsShow: {
           min: ['name', 'actions'],
           default: [
-            'name', 'command_groups', 'priority',
+            'name', 'command_groups_amount', 'priority',
             'is_active', 'comment', 'actions'
           ]
         },
@@ -42,11 +42,11 @@ export default {
               route: 'CommandFilterACLDetail'
             }
           },
-          command_groups: {
+          command_groups_amount: {
             width: '160px',
             formatter: AmountFormatter,
             formatterArgs: {
-              route: 'AccountGatherList',
+              async: true,
               getRoute({ row }) {
                 return {
                   name: 'CommandFilterACLList',
