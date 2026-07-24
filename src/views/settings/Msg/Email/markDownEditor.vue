0f42b02e143a219b327ff75727bc7a3b3cf7d20d
w940853815 <940853815@qq.com>
2026-07-24 15:50:45 +0800

    perf: restore message template editor toolbar

--- src/views/settings/Msg/Email/markDownEditor.vue
+++ src/views/settings/Msg/Email/markDownEditor.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="markdown-editor">
-    <div class="action-bar">
-      <i class="fa" :class="[showPreview ? 'fa-eye-slash' : 'fa-eye']" @click="togglePreview" />
-    </div>
-    <div class="editor-scroll">
-      <el-row :gutter="12" :class="{ 'is-split': showPreview }" class="editor-panes">
-        <el-col :span="showPreview ? 12 : 24" class="editor-pane">
-          <el-input
-            v-model="localValue"
-            :autosize="{ minRows: 16 }"
-            class="editor-input"
-            type="textarea"
-          />
-        </el-col>
-        <el-col v-show="showPreview" :span="12" class="editor-pane">
-          <div class="preview markdown-body" v-html="html" />
-        </el-col>
-      </el-row>
-    </div>
+    <VueMarkdownEditor
+      v-model="localValue"
+      :left-toolbar="leftToolbar"
+      :right-toolbar="rightToolbar"
+      height="400px"
+    />
   </div>
 </template>
+
 <script>
+import VueMarkdownEditor from '@kangc/v-md-editor'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
 import DOMPurify from 'dompurify'
 import MarkdownIt from 'markdown-it'
+import Prism from 'prismjs'
+
+VueMarkdownEditor.use(vuepressTheme, { Prism })
 
 const markdown = new MarkdownIt({
   html: true,
@@ -37,6 +33,7 @@ function renderHtml(source) {
 
 export default {
   name: 'RichEditor',
+  components: { VueMarkdownEditor },
   props: {
     value: {
       type: String,
@@ -46,7 +43,8 @@ export default {
   data() {
     return {
       localValue: this.value,
-      showPreview: true
+      leftToolbar: 'undo redo clear | h bold italic strikethrough quote | ul ol hr | link code',
+      rightToolbar: 'preview sync-scroll fullscreen'
     }
   },
   computed: {
@@ -67,75 +65,16 @@ export default {
   },
   mounted() {
     this.$emit('htmlChange', this.html)
-  },
-  methods: {
-    togglePreview() {
-      this.showPreview = !this.showPreview
-    }
   }
 }
 </script>
+
 <style lang="scss" scoped>
 .markdown-editor {
-  // 表单项内容区是 flex 容器,不设宽度会按内容收缩成窄条,这里撑满可用宽度
   width: 100%;
   min-width: 0;
-  max-width: 100%;
+  border: 1px solid #ccc;
   box-sizing: border-box;
-  position: relative;
   padding: 10px;
-  border: 1px solid #dcdfe6;
-  border-radius: 4px;
-}
-
-.action-bar {
-  position: absolute;
-  top: 10px;
-  right: 12px;
-  z-index: 1;
-
-  i {
-    cursor: pointer;
-  }
-}
-
-.editor-scroll {
-  width: 100%;
-  min-width: 0;
-  overflow-x: hidden;
-}
-
-.editor-panes {
-  flex-wrap: nowrap;
-}
-
-.editor-panes.is-split {
-  flex-wrap: wrap;
-  row-gap: 12px;
-}
-
-.editor-pane {
-  min-width: 0;
-}
-
-.editor-panes.is-split .editor-pane {
-  flex: 1 1 280px;
-  width: auto;
-  max-width: 100%;
-}
-
-.editor-input :deep(.el-textarea__inner) {
-  min-height: 400px !important;
-  font-family: Monaco, Menlo, Consolas, 'Courier New', monospace;
-}
-
-.preview {
-  min-height: 400px;
-  padding: 12px;
-  overflow: auto;
-  border: 1px solid #dcdfe6;
-  border-radius: 4px;
-  background: #fff;
-  @import 'github-markdown-css/github-markdown-light.css';
 }
 </style>
