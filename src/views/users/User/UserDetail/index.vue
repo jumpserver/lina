5434cc377a5ef5747be5658f964457d4e61707be
zhaojisen <1301338853@qq.com>
2026-07-27 13:59:26 +0800

    fix: fix some issues

--- src/views/users/User/UserDetail/index.vue
+++ src/views/users/User/UserDetail/index.vue
@@ -85,10 +85,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters([
-        'currentUserIsSuperAdmin',
-        'publicSettings'
-      ])
+    ...mapGetters(['currentUserIsSuperAdmin', 'publicSettings'])
   },
   methods: {
     handleTabClick(tab) {
