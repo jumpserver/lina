1239e305b542d25a82f699fba8db3efbd37a25ad
feng <1304903146@qq.com>
2026-07-28 11:25:13 +0800

    perf: jdmc sso login

--- src/utils/jdmc.js
+++ src/utils/jdmc.js
@@ -1,3 +1,38 @@
+import i18n from '@/i18n/i18n'
+import { BASE_URL } from '@/utils/common/index'
+import request from '@/utils/request'
+import { message } from '@/utils/vue/message'
+
 export function openJDMC(path) {
-  window.open(path, '_blank')
+  const targetWindow = window.open('', '_blank')
+
+  return request({
+    url: '/api/v1/common/jdmc/sso-token/',
+    method: 'get',
+    disableFlashErrorMsg: true
+  })
+    .then((response) => {
+      const params = new URLSearchParams({
+        token: response.token,
+        next: path
+      })
+      let url = `${BASE_URL}/jdmc/api/v1/auth/tokens?${params.toString()}`
+      if (process.env.NODE_ENV !== 'production') {
+        url = url.replace('9528', '9898')
+      }
+
+      if (targetWindow) {
+        targetWindow.location.replace(url)
+      } else {
+        window.open(url, '_blank')
+      }
+    })
+    .catch((error) => {
+      targetWindow?.close()
+      if (error?.response?.status === 403) {
+        message.error(error?.message || i18n.t('BadRoleErrorMsg'))
+        return
+      }
+      message.error(error?.message || i18n.t('BadRequestErrorMsg'))
+    })
 }
