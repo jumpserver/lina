bdf7f4a921aef6051eba438495780a5d58bca904
zhaojisen <1301338853@qq.com>
2026-08-10 14:43:36 +0800

    fix(setting&site_message): fix setting navigate to blank view and site message info a link target

--- src/layout/components/NavHeader/SystemSetting.vue
+++ src/layout/components/NavHeader/SystemSetting.vue
@@ -1,16 +1,22 @@
 <template>
   <el-link @click="goto()">
-    <svg-icon icon-class="nav-setting" @click="goto()" />
+    <svg-icon icon-class="nav-setting" />
   </el-link>
 </template>
 
 <script>
+import { getFirstAccessibleChildPath } from '@/utils/vue'
+
 export default {
   name: 'WebTerminal',
   computed: {},
   methods: {
     goto() {
-      this.$router.push({ name: 'SystemSetting' })
+      const path = getFirstAccessibleChildPath('/settings') || '/settings/basic'
+      if (this.$route.path === path) {
+        return
+      }
+      this.$router.push(path)
     }
   }
 }
