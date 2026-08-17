72c66d319f2a6350374d12f1ab2dd7a4963014dc
TingRuDeng <DengM891@yeah.net>
2026-08-17 12:14:32 +0800

    fix(account): emit submit event for push parameters (#5617)

--- src/views/assets/Platform/AutomationParamsSetting.vue
+++ src/views/assets/Platform/AutomationParamsSetting.vue
@@ -40,6 +40,7 @@ export default {
     Dialog,
     AutoDataForm
   },
+  emits: ['input', 'submit', 'update:visible', 'canSetting'],
   props: {
     value: {
       type: [String, Object],
@@ -186,6 +187,7 @@ export default {
     },
     onSubmit(form) {
       this.$emit('input', form)
+      this.$emit('submit', form)
       this.isVisible = false
       this.$emit('update:visible', this.isVisible)
     },
