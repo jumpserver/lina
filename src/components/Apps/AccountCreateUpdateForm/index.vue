8b2b114c4da89767e8a4f7972ee26c5d083aa33c
ibuler <ibuler@qq.com>
2026-09-22 16:17:41 +0800

    fix: confirm credential replacement when enabling template following

--- src/components/Apps/AccountCreateUpdateForm/index.vue
+++ src/components/Apps/AccountCreateUpdateForm/index.vue
@@ -150,10 +150,27 @@ export default {
         return secretTypes.indexOf(item.value) > -1
       })
     },
-    confirm(form) {
+    async confirm(form) {
+      if (this.account?.name && !this.account.follow_template && form.follow_template) {
+        try {
+          await this.$confirm(this.$t('FollowTemplateEnableConfirm'), this.$t('Info'), {
+            confirmButtonText: this.$t('Confirm'),
+            cancelButtonText: this.$t('Cancel'),
+            type: 'warning',
+            closeOnClickModal: false
+          })
+        } catch {
+          return
+        }
+      }
       const secretType = form.secret_type || 'password'
-      form.secret = form[secretType]
-      form.secret = this.encryptPassword ? encryptPassword(form.secret) : form.secret
+      if (form.follow_template) {
+        delete form.secret
+        delete form.passphrase
+      } else {
+        form.secret = form[secretType]
+        form.secret = this.encryptPassword ? encryptPassword(form.secret) : form.secret
+      }
 
       // 如果不删除会明文显示
       delete form[secretType]
